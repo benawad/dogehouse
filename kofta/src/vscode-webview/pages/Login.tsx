b111a4bf3d125afda8df9b423a161d13dc7d5491
Ben Awad <benawad97@gmail.com>
2021-02-11 07:59:17 -0600

    login footer

--- kofta/src/vscode-webview/pages/Login.tsx
+++ kofta/src/vscode-webview/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { tw } from "twind";
 import { Button } from "../components/Button";
+import { Footer } from "../components/Footer";
 import { Wrapper } from "../components/Wrapper";
 import { apiBaseUrl } from "../constants";
 import { Logo } from "../svgs/Logo";
@@ -10,46 +11,59 @@ interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
   return (
-    <Wrapper>
-      <div className={tw`mb-8`}>
-        <Logo />
-      </div>
-      <div className={tw`text-2xl`}>
-        An audio livestreaming social network that may or may not be a Clubhouse
-        clone with:
-      </div>
-      <ul className={tw`my-4 mb-8 text-xl`}>
-        <li>- Less features</li>
-        <li>- Dark theme</li>
-        <li>- Open sign ups</li>
-        <li>- Cross platform support</li>
-      </ul>
-      <Button
-        onClick={() => (window.location.href = apiBaseUrl + "/auth/github/web")}
-      >
-        login with Github
-      </Button>
-      {process.env.NODE_ENV === "development" ? (
+    <>
+      <Wrapper>
+        <div className={tw`mb-8`}>
+          <Logo />
+        </div>
+        <div className={tw`text-2xl`}>
+          An audio livestreaming social network that may or may not be a
+          Clubhouse clone with:
+        </div>
+        <ul className={tw`my-4 mb-8 text-xl`}>
+          <li>- Less features</li>
+          <li>- Dark theme</li>
+          <li>- Open sign ups</li>
+          <li>- Cross platform support</li>
+          <li>- Powered by Doge</li>
+        </ul>
         <Button
-          style={{ marginTop: 10 }}
-          onClick={async () => {
-            const name = window.prompt("username");
-            if (!name) {
-              return;
-            }
-            const r = await fetch(
-              `${apiBaseUrl}/dev/test-info?username=` + name
-            );
-            const d = await r.json();
-            useTokenStore.getState().setTokens({
-              accessToken: d.accessToken,
-              refreshToken: d.refreshToken,
-            });
-          }}
+          onClick={() =>
+            (window.location.href = apiBaseUrl + "/auth/github/web")
+          }
         >
-          create test user
+          login with Github
         </Button>
-      ) : null}
-    </Wrapper>
+        {process.env.NODE_ENV === "development" ? (
+          <Button
+            style={{ marginTop: 10 }}
+            onClick={async () => {
+              const name = window.prompt("username");
+              if (!name) {
+                return;
+              }
+              const r = await fetch(
+                `${apiBaseUrl}/dev/test-info?username=` + name
+              );
+              const d = await r.json();
+              useTokenStore.getState().setTokens({
+                accessToken: d.accessToken,
+                refreshToken: d.refreshToken,
+              });
+            }}
+          >
+            create test user
+          </Button>
+        ) : null}
+      </Wrapper>
+      <div
+        style={{
+          padding: "0 var(--container-paddding)",
+        }}
+        className={tw`mb-6`}
+      >
+        <Footer />
+      </div>
+    </>
   );
 };
