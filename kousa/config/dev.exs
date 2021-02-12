f5b103e82cbb65abb3ea1d7915c5b7c763525672
Jakub Perzylo <jakub.perzylo@swmansion.com>
2021-02-12 21:02:03 +0100

    Uncommented dev config

--- kousa/config/dev.exs
+++ kousa/config/dev.exs
@@ -8,38 +8,37 @@ config :kousa, Beef.Repo,
 
 config :kousa,
   web_url: "http://localhost:3000",
-  api_url: "http://localhost:4001"
+  api_url: "http://localhost:4001",
+  ben_github_id:
+    System.get_env("BEN_GITHUB_ID") ||
+      raise("""
+      environment variable BEN_GITHUB_ID is missing.
+      """),
+  access_token_secret:
+    System.get_env("ACCESS_TOKEN_SECRET") ||
+      raise("""
+      environment variable ACCESS_TOKEN_SECRET is missing.
+      type some random characters to create one
+      """),
+  refresh_token_secret:
+    System.get_env("REFRESH_TOKEN_SECRET") ||
+      raise("""
+      environment variable REFRESH_TOKEN_SECRET is missing.
+      type some random characters to create one
+      """),
+  client_id:
+    System.get_env("GITHUB_CLIENT_ID") ||
+      raise("""
+      environment variable GITHUB_CLIENT_ID is missing.
+      Create an oauth application on GitHub to get one
+      """),
+  client_secret:
+    System.get_env("GITHUB_CLIENT_SECRET") ||
+      raise("""
+      environment variable GITHUB_CLIENT_SECRET is missing.
+      Create an oauth application on GitHub to get one
+      """)
 
-# ben_github_id:
-#   System.get_env("BEN_GITHUB_ID") ||
-#     raise("""
-#     environment variable BEN_GITHUB_ID is missing.
-#     """),
-# access_token_secret:
-#   System.get_env("ACCESS_TOKEN_SECRET") ||
-#     raise("""
-#     environment variable ACCESS_TOKEN_SECRET is missing.
-#     type some random characters to create one
-#     """),
-# refresh_token_secret:
-#   System.get_env("REFRESH_TOKEN_SECRET") ||
-#     raise("""
-#     environment variable REFRESH_TOKEN_SECRET is missing.
-#     type some random characters to create one
-#     """),
-# client_id:
-#   System.get_env("GITHUB_CLIENT_ID") ||
-#     raise("""
-#     environment variable GITHUB_CLIENT_ID is missing.
-#     Create an oauth application on GitHub to get one
-#     """),
-# client_secret:
-#   System.get_env("GITHUB_CLIENT_SECRET") ||
-#     raise("""
-#     environment variable GITHUB_CLIENT_SECRET is missing.
-#     Create an oauth application on GitHub to get one
-#     """)
-
-# config :joken,
-#   access_token_secret: System.fetch_env!("ACCESS_TOKEN_SECRET"),
-#   refresh_token_secret: System.fetch_env!("REFRESH_TOKEN_SECRET")
+config :joken,
+  access_token_secret: System.fetch_env!("ACCESS_TOKEN_SECRET"),
+  refresh_token_secret: System.fetch_env!("REFRESH_TOKEN_SECRET")
