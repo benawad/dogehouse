2de4aa74b4c24190f0af6f8d9df755aad154f666
Ben Awad <benawad97@gmail.com>
2021-02-12 18:06:10 -0600

    put token logic back

--- kousa/lib/tokens/refresh_token.ex
+++ kousa/lib/tokens/refresh_token.ex
@@ -1,6 +1,9 @@
 defmodule Kousa.RefreshToken do
-  use Joken.Config, default_signer: :refresh_token_secret
+  def __default_signer__,
+    do: Joken.Signer.create("HS256", Application.fetch_env!(:kousa, :refresh_token_secret))
+
+  use Joken.Config
 
   # 30 days
-  def token_config(), do: default_claims(default_exp: 60 * 60 * 24 * 30)
+  def token_config, do: default_claims(default_exp: 60 * 60 * 24 * 30)
 end
