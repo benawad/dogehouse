cdacb51edca6bc48d88c697396c1135a166f58da
arhammusheer <arhammusheer@gmail.com>
2021-02-16 01:10:13 +0530

    Electron App wrapper
    Added electron app wrapper to dogehouse

---
+++ falafel/src/electron.ts
@@ -0,0 +1,40 @@
+import { BrowserWindow, app, systemPreferences, ipcMain } from "electron";
+import * as path from "path";
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 560,
+    height: 1000,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  console.log(systemPreferences.getMediaAccessStatus("microphone"));
+  mainWindow.loadURL(
+    `https://dogehouse.tv/`
+  );
+
+  ipcMain.on("request-mic", async (event, serviceName) => {
+    const isAllowed: boolean = await systemPreferences.askForMediaAccess(
+      "microphone"
+    );
+    event.returnValue = isAllowed;
+  });
+  mainWindow.on("closed", () => mainWindow.destroy());
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
