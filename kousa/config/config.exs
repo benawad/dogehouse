165a5e9cc071be5bca08e831be35606cec26b9c2
Ben Awad <benawad97@gmail.com>
2021-02-20 20:12:47 -0600

    switch to ueberauth for more consistent twitter logins

--- kousa/config/config.exs
+++ kousa/config/config.exs
@@ -5,3 +5,9 @@ config :kousa, max_room_size: 300
 config :extwitter, :json_library, Poison
 
 import_config "#{Mix.env()}.exs"
+
+# config :ueberauth, Ueberauth,
+#   providers: [
+#     github: {Ueberauth.Strategy.Github, [default_scope: "user,user:email"]}
+#   ],
+#   json_library: Poison
