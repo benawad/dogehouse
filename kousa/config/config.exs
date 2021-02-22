1a88da028a9308cc2e070b543e6cb06e14cdc660
Ben Awad <benawad97@gmail.com>
2021-02-21 20:08:18 -0600

    switch back to 1 vc until we need more

--- kousa/config/config.exs
+++ kousa/config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :kousa, ecto_repos: [Beef.Repo]
-config :kousa, max_room_size: 1000, num_voice_servers: 2
+config :kousa, max_room_size: 1000
 config :extwitter, :json_library, Poison
 
 import_config "#{Mix.env()}.exs"
