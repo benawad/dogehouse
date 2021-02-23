2a397319073fd11c2b6ad9867615f2a241714915
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2021-02-23 21:30:26 +0530

    Fixed some merge issues

--- feta/constants.ts
+++ feta/constants.ts
@@ -3,3 +3,5 @@ export const __prod__ = process.env.NODE_ENV === "production";
 export const apiBaseUrl =
   process.env.REACT_APP_API_BASE_URL ||
   (__prod__ ? "https://api.dogehouse.tv" : "http://localhost:4001");
+
+export const linkRegex = /(https?:\/\/|)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
