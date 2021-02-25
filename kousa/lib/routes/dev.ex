d12fc7966275b64fcda362c04922076967e9f7c1
Ben Awad <benawad97@gmail.com>
2021-02-25 11:04:36 -0600

    let you create test users in staging

--- kousa/lib/routes/dev.ex
+++ kousa/lib/routes/dev.ex
@@ -7,7 +7,8 @@ defmodule Kousa.Dev do
   plug(:dispatch)
 
   get "/test-info" do
-    if Application.fetch_env!(:kousa, :env) != :dev do
+    if Application.fetch_env!(:kousa, :env) != :dev and
+         not Kousa.Caster.bool(Application.get_env(:kousa, :is_staging)) do
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(400, Poison.encode!(%{"error" => "no"}))
