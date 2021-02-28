b8ccfbf664cc397531eb738e54e96aa684ff4aa1
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-02-28 19:18:28 +0500

    Kousa: Optimze whisper code

--- kousa/lib/gen/room_chat.ex
+++ kousa/lib/gen/room_chat.ex
@@ -65,17 +65,19 @@ defmodule Kousa.Gen.RoomChat do
 
     if not Map.has_key?(state.ban_map, user_id) and
       (is_nil(last_timestamp) or seconds - last_timestamp > 0) do
-      users = if whispered_to != [], do: Enum.filter(state.users, fn uid -> Enum.member?([user_id] ++ whispered_to, uid) end), else: state.users
-      ws_fan(users, :chat, %{
-        op: "new_chat_msg",
-        d: %{
-          userId: user_id,
-          msg: msg
-        }
-      })
 
-      {:noreply,
-       %State{state | last_message_map: Map.put(state.last_message_map, user_id, seconds)}}
+        whispered_to_users_list = [user_id | whispered_to]
+        users = if whispered_to != [], do: Enum.filter(state.users, fn uid -> Enum.member?(whispered_to_users_list, uid) end), else: state.users
+        ws_fan(users, :chat, %{
+          op: "new_chat_msg",
+          d: %{
+            userId: user_id,
+            msg: msg
+          }
+        })
+
+        {:noreply,
+        %State{state | last_message_map: Map.put(state.last_message_map, user_id, seconds)}}
     else
       {:noreply, state}
     end
