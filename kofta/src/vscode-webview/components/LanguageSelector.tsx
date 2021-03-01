d82aaf5b4f5bc1f1b594c32345d890b71c0859c3
Ermal Shkullaku <ermal.shkullaku@levrx.com>
2021-02-28 22:38:22 -0500

    Added support for #488 - UI Internationalization: - i18next integration - created translation select dropdown component - implemented footer translations for es, en, de, fr Added dependencies/libraries: - i18next - react-i18next - react-dropdown-select TODO: - further translations (need help w/ this)

---
+++ kofta/src/vscode-webview/components/LanguageSelector.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Select from "react-dropdown-select";
+import { useTranslation } from "react-i18next";
+
+interface LanguageSelectorProps<T> {
+    values?: T[];
+    options?: T[];
+}
+
+const dropDownStyles = {
+    color: "#000000",
+    backgroundColor: "#FFFFFF"
+}
+
+const defaultValue = { value: "en", label: "en", style: dropDownStyles };
+
+export const LanguageSelector: React.FC<LanguageSelectorProps<{}>> = ({
+    values = [defaultValue],
+    options = [
+        { value: "en", label: "en" },
+        { value: "de", label: "de" },
+        { value: "es", label: "es" },
+        { value: "fr", label: "fr" }
+    ]
+}) => {
+    const { i18n } = useTranslation();
+    const changeLanguage = (lng: any) => {
+        i18n.changeLanguage(lng[0].value);
+    };
+
+    return (
+        <Select
+            options={options}
+            values={values}
+            onChange={values => changeLanguage(values)}
+            style={dropDownStyles}
+            dropdownPosition={"top"}
+        />
+    );
+};
