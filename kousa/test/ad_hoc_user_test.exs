47af52b927f1fc0a2d025e0ac2f5a4fd715c0700
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-02-28 19:52:58 -0800

    (failing) test, except for the room factory

--- kousa/test/ad_hoc_user_test.exs
+++ kousa/test/ad_hoc_user_test.exs
@@ -41,6 +41,27 @@ defmodule KousaTest.AdHocUserTest do
   end
 
   describe "Beef.RoomBlock" do
+    alias Beef.RoomBlock
+    alias Beef.Room
+
+    test "you can add a room blocker into the roomblock table" do
+      %{id: uid} = Factory.create(User)
+      %{id: rid} = Factory.create(Room)
+      %{id: mid} = Factory.create(User)
+
+      assert {:ok, %RoomBlock{userId: ^uid, roomId: ^rid, modId: ^mid}} =
+        RoomBlock.insert_changeset(%RoomBlock{}, %{userId: uid, roomId: rid, modId: mid})
+
+      assert [room] = Repo.all(RoomBlock)
+      assert %RoomBlock{
+        userId: ^uid,
+        user: %User{id: ^uid},
+        roomId: ^rid,
+        # TODO: insert room assoc here.
+        modId: ^mid,
+        mod: %User{id: ^mid}
+      } = Repo.preload(room, [:user, :mod])
+    end
   end
 
   describe "Beef.Room" do
