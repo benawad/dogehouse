a1a7ef96982c81e24e4f47646a555ccd93231bd3
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-03-01 22:26:02 -0800

    fix up Kousa.BL.Room

--- kousa/lib/business-logic/room_logic.ex
+++ kousa/lib/business-logic/room_logic.ex
@@ -1,14 +1,18 @@
 defmodule Kousa.BL.Room do
   use Kousa.Dec.Atomic
   alias Kousa.BL
-  alias Kousa.Data
   alias Kousa.RegUtils
   alias Kousa.Gen
   alias Kousa.Caster
   alias Kousa.VoiceServerUtils
+  # note the following three module aliases are on the chopping block!
+  alias Kousa.Data.Follower
+  alias Kousa.Data.Room
+  alias Kousa.Data.RoomPermission
+  alias Beef.Users
 
   def set_auto_speaker(user_id, value) do
-    room = Kousa.Data.Room.get_room_by_creator_id(user_id)
+    room = Room.get_room_by_creator_id(user_id)
 
     if not is_nil(room) do
       Kousa.RegUtils.lookup_and_cast(Kousa.Gen.RoomSession, room.id, {:set_auto_speaker, value})
@@ -18,7 +22,7 @@ defmodule Kousa.BL.Room do
   @spec make_room_public(any, any) :: nil | :ok
   def make_room_public(user_id, new_name) do
     # this needs to be refactored if a user can have multiple rooms
-    case Kousa.Data.Room.set_room_privacy_by_creator_id(user_id, false, new_name) do
+    case Room.set_room_privacy_by_creator_id(user_id, false, new_name) do
       {1, [room]} ->
         Kousa.Gen.RoomSession.send_cast(
           room.id,
@@ -34,7 +38,7 @@ defmodule Kousa.BL.Room do
   @spec make_room_private(any, any) :: nil | :ok
   def make_room_private(user_id, new_name) do
     # this needs to be refactored if a user can have multiple rooms
-    case Kousa.Data.Room.set_room_privacy_by_creator_id(user_id, true, new_name) do
+    case Room.set_room_privacy_by_creator_id(user_id, true, new_name) do
       {1, [room]} ->
         Kousa.Gen.RoomSession.send_cast(
           room.id,
@@ -48,12 +52,12 @@ defmodule Kousa.BL.Room do
   end
 
   def invite_to_room(user_id, user_id_to_invite) do
-    user = Data.User.get_by_id(user_id)
+    user = Users.get_by_id(user_id)
 
     if not is_nil(user.currentRoomId) and
-         Data.Follower.is_following_me(user_id, user_id_to_invite) do
+         Follower.is_following_me(user_id, user_id_to_invite) do
       # @todo store room name in RoomSession to avoid db lookups
-      room = Data.Room.get_room_by_id(user.currentRoomId)
+      room = Room.get_room_by_id(user.currentRoomId)
 
       if not is_nil(room) do
         Gen.RoomSession.send_cast(
@@ -73,15 +77,15 @@ defmodule Kousa.BL.Room do
 
   def block_from_room(user_id, user_id_to_block_from_room) do
     with {status, room} when status in [:creator, :mod] <-
-           Kousa.Data.Room.get_room_status(user_id) do
+           Room.get_room_status(user_id) do
       if room.creatorId != user_id_to_block_from_room do
-        Kousa.Data.RoomBlock.insert(%{
+        RoomBlock.insert(%{
           modId: user_id,
           userId: user_id_to_block_from_room,
           roomId: room.id
         })
 
-        user_blocked = Kousa.Data.User.get_by_id(user_id_to_block_from_room)
+        user_blocked = Users.get_by_id(user_id_to_block_from_room)
 
         if user_blocked.currentRoomId == room.id do
           leave_room(user_id_to_block_from_room, user_blocked.currentRoomId, true)
@@ -91,7 +95,7 @@ defmodule Kousa.BL.Room do
   end
 
   defp internal_set_listener(user_id_to_make_listener, room_id) do
-    Data.RoomPermission.make_listener(user_id_to_make_listener, room_id)
+    RoomPermission.make_listener(user_id_to_make_listener, room_id)
 
     Kousa.RegUtils.lookup_and_cast(
       Kousa.Gen.RoomSession,
@@ -104,16 +108,16 @@ defmodule Kousa.BL.Room do
     if user_id == user_id_to_set_listener do
       internal_set_listener(
         user_id_to_set_listener,
-        Kousa.Data.User.get_current_room_id(user_id_to_set_listener)
+        Users.get_current_room_id(user_id_to_set_listener)
       )
     else
-      {status, room} = Kousa.Data.Room.get_room_status(user_id)
+      {status, room} = Room.get_room_status(user_id)
       is_creator = user_id_to_set_listener == not is_nil(room) and room.creatorId
 
       if not is_creator and (status == :creator or status == :mod) do
         internal_set_listener(
           user_id_to_set_listener,
-          Kousa.Data.User.get_current_room_id(user_id_to_set_listener)
+          Users.get_current_room_id(user_id_to_set_listener)
         )
       end
     end
@@ -122,7 +126,7 @@ defmodule Kousa.BL.Room do
   @spec internal_set_speaker(any, any) :: nil | :ok | {:err, {:error, :not_found}}
   def internal_set_speaker(user_id_to_make_speaker, room_id) do
     with {:ok, _} <-
-           Kousa.Data.RoomPermission.set_is_speaker(user_id_to_make_speaker, room_id, true) do
+           RoomPermission.set_is_speaker(user_id_to_make_speaker, room_id, true) do
       case GenRegistry.lookup(
              Kousa.Gen.RoomSession,
              room_id
@@ -142,16 +146,16 @@ defmodule Kousa.BL.Room do
 
   def make_speaker(user_id, user_id_to_make_speaker) do
     with {status, room} when status in [:creator, :mod] <-
-           Kousa.Data.Room.get_room_status(user_id) do
+           Room.get_room_status(user_id) do
       internal_set_speaker(user_id_to_make_speaker, room.id)
     end
   end
 
   def change_mod(user_id, user_id_to_change, value) do
-    room = Kousa.Data.Room.get_room_by_creator_id(user_id)
+    room = Room.get_room_by_creator_id(user_id)
 
     if room do
-      Kousa.Data.RoomPermission.set_is_mod(user_id_to_change, room.id, Caster.bool(value))
+      RoomPermission.set_is_mod(user_id_to_change, room.id, Caster.bool(value))
 
       Kousa.RegUtils.lookup_and_cast(
         Kousa.Gen.RoomSession,
@@ -170,7 +174,7 @@ defmodule Kousa.BL.Room do
       if is_nil(is_speaker),
         do:
           room.creatorId == user_id or
-            Kousa.Data.RoomPermission.is_speaker(user_id, room.id),
+            RoomPermission.is_speaker(user_id, room.id),
         else: is_speaker
 
     op =
@@ -190,8 +194,8 @@ defmodule Kousa.BL.Room do
   end
 
   def rename_room(user_id, new_name) do
-    with {:ok, room_id} <- Data.User.tuple_get_current_room_id(user_id),
-         {1, _} <- Data.Room.update_name(user_id, new_name) do
+    with {:ok, room_id} <- Users.tuple_get_current_room_id(user_id),
+         {1, _} <- Room.update_name(user_id, new_name) do
       nil
       RegUtils.lookup_and_cast(Gen.RoomSession, room_id, {:new_room_name, new_name})
     end
@@ -199,7 +203,7 @@ defmodule Kousa.BL.Room do
 
   # @decorate user_atomic()
   def create_room(user_id, room_name, is_private) do
-    room_id = Kousa.Data.User.get_current_room_id(user_id)
+    room_id = Users.get_current_room_id(user_id)
 
     if not is_nil(room_id) do
       leave_room(user_id, room_id)
@@ -207,7 +211,7 @@ defmodule Kousa.BL.Room do
 
     id = Ecto.UUID.generate()
 
-    case Data.Room.create(%{
+    case Room.create(%{
            id: id,
            name: room_name,
            creatorId: user_id,
@@ -250,12 +254,12 @@ defmodule Kousa.BL.Room do
 
   # @decorate user_atomic()
   def join_room(user_id, room_id) do
-    currentRoomId = Kousa.Data.User.get_current_room_id(user_id)
+    currentRoomId = Users.get_current_room_id(user_id)
 
     if currentRoomId == room_id do
-      %{room: Kousa.Data.Room.get_room_by_id(room_id)}
+      %{room: Room.get_room_by_id(room_id)}
     else
-      case Kousa.Data.Room.can_join_room(room_id, user_id) do
+      case Room.can_join_room(room_id, user_id) do
         {:error, message} ->
           %{error: message}
 
@@ -284,7 +288,7 @@ defmodule Kousa.BL.Room do
                 leave_room(user_id, currentRoomId)
               end
 
-              updated_user = Kousa.Data.Room.join_room(room, user_id)
+              updated_user = Room.join_room(room, user_id)
 
               Kousa.Gen.RoomSession.send_cast(
                 room_id,
@@ -310,11 +314,11 @@ defmodule Kousa.BL.Room do
   def leave_room(user_id, current_room_id \\ nil, blocked \\ false) do
     current_room_id =
       if is_nil(current_room_id),
-        do: Kousa.Data.User.get_current_room_id(user_id),
+        do: Users.get_current_room_id(user_id),
         else: current_room_id
 
     if current_room_id do
-      case Kousa.Data.Room.leave_room(user_id, current_room_id) do
+      case Room.leave_room(user_id, current_room_id) do
         # the room should be destroyed
         {:bye, room} ->
           Kousa.Gen.RoomSession.send_cast(current_room_id, {:destroy, user_id})
