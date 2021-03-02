4609fd382d4d60f7cef87988875b5ceb68f0dc5c
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-03-01 23:10:15 -0800

    merge conflict resolution

---
+++ kousa/test/_support/ecto_sandbox.ex
@@ -0,0 +1,19 @@
+defmodule Kousa.Support.EctoSandbox do
+  # see https://hexdocs.pm/ecto/testing-with-ecto.html
+
+  defmacro __using__(_) do
+    quote do
+      def checkout_ecto_sandbox(tags) do
+        :ok = Ecto.Adapters.SQL.Sandbox.checkout(Beef.Repo)
+
+        unless tags[:async] do
+          Ecto.Adapters.SQL.Sandbox.mode(Beef.Repo, {:shared, self()})
+        end
+
+        :ok
+      end
+
+      setup :checkout_ecto_sandbox
+    end
+  end
+end
