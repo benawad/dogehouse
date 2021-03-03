73659177f63c7a1175e07ac87940796e1e4da3e4
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-03-04 03:45:19 +0500

    Add MAX_COLLAPSED_CHARACTERS

--- kofta/src/vscode-webview/modules/room-chat/RoomDescription.tsx
+++ kofta/src/vscode-webview/modules/room-chat/RoomDescription.tsx
@@ -7,17 +7,20 @@ interface RoomDescriptionProps {}
 export const RoomDescription: React.FC<RoomDescriptionProps> = () => {
   const { currentRoom } = useCurrentRoomStore();
   const [expanded, setExpanded] = useState(false);
+  const MAX_COLLAPSED_CHARACTERS = 100;
 
   return currentRoom?.description ? (
     <div className="p-3 rounded-lg m-3 bg-simple-gray-3a">
       <p className="text-gray-400 mb-1">room description</p>
       <p>
-        {expanded ? currentRoom.description : truncate(currentRoom.description)}
+        {expanded
+          ? currentRoom.description
+          : truncate(currentRoom.description, MAX_COLLAPSED_CHARACTERS)}
         <button
           className="ml-1 text-blue-400 cursor-pointer hover:text-blue-300"
           onClick={() => setExpanded(!expanded)}
         >
-          {currentRoom.description?.length > 100
+          {currentRoom.description?.length > MAX_COLLAPSED_CHARACTERS
             ? "show " + (expanded ? "less" : "more")
             : null}
         </button>
