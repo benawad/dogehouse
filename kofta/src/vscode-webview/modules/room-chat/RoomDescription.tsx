12c3f86e532ec46adb01a6b2969e5aaf5224adda
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-03-04 22:45:47 +0500

    room desc break all

--- kofta/src/vscode-webview/modules/room-chat/RoomDescription.tsx
+++ kofta/src/vscode-webview/modules/room-chat/RoomDescription.tsx
@@ -5,26 +5,26 @@ import { truncate } from "../../utils/truncate";
 interface RoomDescriptionProps {}
 
 export const RoomDescription: React.FC<RoomDescriptionProps> = () => {
-  const { currentRoom } = useCurrentRoomStore();
-  const [expanded, setExpanded] = useState(false);
-  const MAX_COLLAPSED_CHARACTERS = 100;
+	const { currentRoom } = useCurrentRoomStore();
+	const [expanded, setExpanded] = useState(false);
+	const MAX_COLLAPSED_CHARACTERS = 100;
 
-  return currentRoom?.description ? (
-    <div className="p-3 rounded-lg m-3 bg-simple-gray-3a">
-      <p className="text-gray-400 mb-1">room description</p>
-      <p>
-        {expanded
-          ? currentRoom.description
-          : truncate(currentRoom.description, MAX_COLLAPSED_CHARACTERS)}
-        <button
-          className="ml-1 text-blue-400 cursor-pointer hover:text-blue-300"
-          onClick={() => setExpanded(!expanded)}
-        >
-          {currentRoom.description?.length > MAX_COLLAPSED_CHARACTERS
-            ? "show " + (expanded ? "less" : "more")
-            : null}
-        </button>
-      </p>
-    </div>
-  ) : null;
+	return currentRoom?.description ? (
+		<div className="p-3 rounded-lg m-3 bg-simple-gray-3a">
+			<p className="text-gray-400 mb-1">room description</p>
+			<p className="break-all">
+				{expanded
+					? currentRoom.description
+					: truncate(currentRoom.description, MAX_COLLAPSED_CHARACTERS)}
+				<button
+					className="ml-1 text-blue-400 cursor-pointer hover:text-blue-300"
+					onClick={() => setExpanded(!expanded)}
+				>
+					{currentRoom.description?.length > MAX_COLLAPSED_CHARACTERS
+						? "show " + (expanded ? "less" : "more")
+						: null}
+				</button>
+			</p>
+		</div>
+	) : null;
 };
