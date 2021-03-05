37b5f9434b5f5a2122cea15a0e93dc860a642780
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-03-05 23:03:43 +0200

    fix #400

--- kofta/src/app/components/EditProfileModal.tsx
+++ kofta/src/app/components/EditProfileModal.tsx
@@ -15,7 +15,7 @@ import { Modal } from "./Modal";
 import { useTypeSafeTranslation } from "../utils/useTypeSafeTranslation";
 
 const profileStruct = object({
-	displayName: size(string(), 2, 50),
+	displayName: size(pattern(string(), /^[^\s]+(\s+[^\s]+)*$/), 2, 50),
 	username: pattern(string(), /^(\w){4,15}$/),
 	bio: size(string(), 0, 160),
 	avatarUrl: pattern(
