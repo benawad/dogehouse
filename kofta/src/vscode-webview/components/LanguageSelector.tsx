b03bd912a8efbdd7078e0e488eab61c35dcc544f
Ben Awad <benawad97@gmail.com>
2021-03-05 08:26:34 -0600

    use regular select for now

--- kofta/src/vscode-webview/components/LanguageSelector.tsx
+++ kofta/src/vscode-webview/components/LanguageSelector.tsx
@@ -1,40 +1,34 @@
 import React from "react";
-import Select from "react-dropdown-select";
 import { useTranslation } from "react-i18next";
 
 interface LanguageSelectorProps<T> {
-    options?: T[];
+	options?: Array<{ value: string; label: string }>;
 }
-
-const dropDownStyles = {
-    color: "#000000",
-    backgroundColor: "#FFFFFF"
-}
-
-const defaultValue = { value: "en", label: "en" };
 
 export const LanguageSelector: React.FC<LanguageSelectorProps<{}>> = ({
-    options = [
-        { value: "en", label: "en" },
-        { value: "de", label: "de" },
-        { value: "es", label: "es" },
-        { value: "fr", label: "fr" },
-        { value: "pt", label: "pt" },
-        { value: "hu", label: "hu" }
-    ]
+	options = [
+		{ value: "en", label: "en" },
+		{ value: "de", label: "de" },
+		{ value: "es", label: "es" },
+		{ value: "fr", label: "fr" },
+		{ value: "pt", label: "pt" },
+		{ value: "hu", label: "hu" },
+	],
 }) => {
-    const { i18n } = useTranslation();
-    const changeLanguage = (lng: any) => {
-        i18n.changeLanguage(lng[0].value);
-    };
+	const { i18n } = useTranslation();
 
-    return (
-        <Select
-            options={options}
-            values={[defaultValue]}
-            onChange={lng => changeLanguage(lng)}
-            style={dropDownStyles}
-            dropdownPosition={"top"}
-        />
-    );
+	return (
+		<select
+			value={i18n.language}
+			onChange={(e) => {
+				i18n.changeLanguage(e.target.value);
+			}}
+		>
+			{options.map((o) => (
+				<option key={o.value} value={o.value}>
+					{o.label}
+				</option>
+			))}
+		</select>
+	);
 };
