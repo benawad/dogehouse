e44466e9b224e22db31dc37352a8d92679ff36d7
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-03-05 11:43:41 +0500

    remove

--- kofta/src/vscode-webview/pages/Home.tsx
+++ kofta/src/vscode-webview/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Calendar } from "react-feather";
 import { useQuery, useQueryClient } from "react-query";
 import { useHistory } from "react-router-dom";
