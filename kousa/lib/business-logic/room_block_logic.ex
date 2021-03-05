f43effd6d18cfca353a2b3a17bbaf49e1479bef5
Ben Awad <benawad97@gmail.com>
2021-03-05 12:02:14 -0600

    fix spellingg

--- kousa/lib/business-logic/room_block_logic.ex
+++ kousa/lib/business-logic/room_block_logic.ex
@@ -1,11 +1,12 @@
 defmodule Kousa.BL.RoomBlock do
   alias Beef.Users
   alias Beef.Rooms
+  alias Kousa.Data.RoomBlock
 
   def unban(user_id, user_id_to_unban) do
     with {:ok, id} <- Users.tuple_get_current_room_id(user_id),
          true <- Rooms.owner?(id, user_id) do
-      RoomsBlock.unban(id, user_id_to_unban)
+      RoomBlock.unban(id, user_id_to_unban)
     end
   end
 
@@ -14,7 +15,7 @@ defmodule Kousa.BL.RoomBlock do
   def get_blocked_users(user_id, offset) do
     with {:ok, id} <- Users.tuple_get_current_room_id(user_id),
          true <- Rooms.owner?(id, user_id) do
-      RoomsBlock.get_blocked_users(id, offset)
+      RoomBlock.get_blocked_users(id, offset)
     end
   end
 end
