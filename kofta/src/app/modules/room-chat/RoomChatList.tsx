08782f2bc85fa07e4d3c0a4690e948e1c6e8de58
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-03-07 00:52:08 +0500

    fix exhaustive deps

--- kofta/src/app/modules/room-chat/RoomChatList.tsx
+++ kofta/src/app/modules/room-chat/RoomChatList.tsx
@@ -41,7 +41,7 @@ export const RoomChatList: React.FC<ChatListProps> = ({}) => {
 		const handleFocus = () => setNewUnreadMessages(false);
 		window.addEventListener("focus", handleFocus, true);
 		return () => window.removeEventListener("focus", handleFocus, true);
-	}, []);
+	});
 
 	return (
 		<div
