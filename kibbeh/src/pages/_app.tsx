e433e6725af99d824358d534de33fe5dca4b87c1
Ben Awad <benawad97@gmail.com>
2021-03-07 16:37:21 -0600

    test if it lets me commit

--- kibbeh/src/pages/_app.tsx
+++ kibbeh/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
+  const bob = 5;
   return <Component {...pageProps} />;
 }
 
