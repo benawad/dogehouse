80750844b9dfda9c22540463f3d26316bb497000
Johannes Andersen <johannes.andersen@signifyd.com>
2021-03-07 22:21:12 +0000

    feat(locales): improve locale support

--- kofta/src/app/components/InviteButton.tsx
+++ kofta/src/app/components/InviteButton.tsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import { Button } from "./Button";
+import { useTypeSafeTranslation } from "../utils/useTypeSafeTranslation";
 
 interface InviteButtonProps {
-  onClick: () => void;
+	onClick: () => void;
 }
 
 export const InviteButton: React.FC<InviteButtonProps> = ({ onClick }) => {
-  const [invited, setInvited] = useState(false);
-  return (
-    <Button
-      onClick={() => {
-        onClick();
-        setInvited(true);
-      }}
-      disabled={invited}
-      variant="small"
-    >
-      {invited ? "invited" : "invite to room"}
-    </Button>
-  );
+	const [invited, setInvited] = useState(false);
+	const { t } = useTypeSafeTranslation();
+
+	return (
+		<Button
+			onClick={() => {
+				onClick();
+				setInvited(true);
+			}}
+			disabled={invited}
+			variant="small"
+		>
+			{invited
+				? t("components.inviteButton.invited")
+				: t("components.inviteButton.inviteToRoom")}
+		</Button>
+	);
 };
