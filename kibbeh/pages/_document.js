4cc92da20161052543c8762d49d9f0705c7d5a1b
Ilya Maximov <mail@overlisted.net>
2021-03-07 19:43:26 +0100

    set up kibbeh

---
+++ kibbeh/pages/_document.js
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head/>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
