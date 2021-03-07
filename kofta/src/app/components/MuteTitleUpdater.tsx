777d40d115b8bfa7b3ef2a31ba49358777a11507
Moses Gitau <gitaumoses4@gmail.com>
2021-03-07 15:21:20 +0300

    Check for current room when updating mute title

--- kofta/src/app/components/MuteTitleUpdater.tsx
+++ kofta/src/app/components/MuteTitleUpdater.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import { useMuteStore } from "../../webrtc/stores/useMuteStore";
 import { useTypeSafeTranslation } from "../utils/useTypeSafeTranslation";
+import { useCurrentRoomStore } from "../../webrtc/stores/useCurrentRoomStore";
 
 interface MuteTitleUpdaterProps {}
 
 export const MuteTitleUpdater: React.FC<MuteTitleUpdaterProps> = ({}) => {
 	const { muted } = useMuteStore();
-	const location = useLocation();
+	const { currentRoom } = useCurrentRoomStore();
 	const { t } = useTypeSafeTranslation();
 	useEffect(() => {
-		if (muted && location.pathname.startsWith('/room')) {
+		if (muted && currentRoom) {
 			document.title = t("header.mutedTitle");
 		} else {
 			document.title = t("header.title");
 		}
-	}, [muted, t, location.pathname]);
+	}, [muted, t, currentRoom]);
 	return null;
 };
