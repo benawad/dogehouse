3d6db3b83c37dcd795a4754f2ef3a58c1a60c5d1
Ben Awad <benawad97@gmail.com>
2021-03-08 09:24:33 -0600

    style lint stuff

--- kibbeh/src/ui/Button.tsx
+++ kibbeh/src/ui/Button.tsx
@@ -1,9 +1,14 @@
 import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
-export type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-HTMLButtonElement>
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
-export const Button: React.FC<ButtonProps
-> = ({ children, ...props }) => {
-  return <button className={`bg-accent p-2`} {...props}>{children}</button>;
+export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  return (
+    <button className={`bg-accent p-2`} {...props}>
+      {children}
+    </button>
+  );
 };
