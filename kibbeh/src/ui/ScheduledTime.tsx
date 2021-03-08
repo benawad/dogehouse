cbd4447e22103c06767d0e273a1cb24bed5972ef
Jeevan M R <14.jeevan@gmail.com>
2021-03-09 00:57:00 +0530

    fix bg color

--- kibbeh/src/ui/ScheduledTime.tsx
+++ kibbeh/src/ui/ScheduledTime.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const ScheduledTime: React.FC = ({ children }) => {
   return (
     <div className="text-primary-200 font-bold">
-      <div className="inline-block mr-2 w-2 h-2 rounded-full bg-primary-200"></div>
+      <div className="inline-block mr-2 w-2 h-2 rounded-full bg-primary-300"></div>
       {children}
     </div>
   );
