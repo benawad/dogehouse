c6b703b69710f740d77604b383bd4d0e19a58b3c
Choongkyu Kim <choongkyu.kim+gh@gmail.com>
2021-03-07 22:29:15 -0800

    Manually run prettier on prettierrc.js to fix tab spacing

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-	trailingComma: "es5",
-	tabWidth: 2,
-	semi: true,
-	singleQuote: false,
-	arrowParens: "always",
-	useTabs: false,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  useTabs: false,
 };
