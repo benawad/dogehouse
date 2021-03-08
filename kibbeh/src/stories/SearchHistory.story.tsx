3ea6b0fc2e13cb4f87aad97795d6a1e45f04c653
Maycon Benito <mayki17321@gmail.com>
2021-03-08 14:02:47 -0300

    SearchHistory component

---
+++ kibbeh/src/stories/SearchHistory.story.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchHistoryComponent, { SearchHistoryProps } from "../ui/SearchHistory";
+
+export default {
+  title: "SearchHistory",
+  argTypes: {
+    onClickToDeleteSearchHistory: { action: "clicked" },
+    searchText: { defaultValue: "javascript" }
+  }
+};
+
+export const SearchHistory = (props: SearchHistoryProps) => {
+  return <SearchHistoryComponent {...props} />;
+};
---
+++ kibbeh/src/ui/SearchHistory.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import SearchIcon from "../icons/sm-solid-search.svg";
+
+export interface SearchHistoryProps {
+  onClickToDeleteSearchHistory: React.MouseEventHandler<HTMLSpanElement>
+  searchText: string
+}
+
+const SearchHistory: React.FC<SearchHistoryProps> = ({ onClickToDeleteSearchHistory, searchText }) => {
+  return (
+    <div className={"flex flex-row bg-primary-900"}>
+      <div className={"flex flex-1"}>
+        <SearchIcon className={"mr-2"} />
+        <span className={"text-primary-300"}>{searchText}</span>
+      </div>
+      <span onClick={onClickToDeleteSearchHistory} className={"text-accent underline cursor-pointer"}>Delete</span>
+    </div>
+  );
+};
+
+export default SearchHistory;
