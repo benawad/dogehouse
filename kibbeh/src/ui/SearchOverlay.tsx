6152f57e986a85e186d48ce5ec33cce086987ce4
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-03-09 21:24:36 +0200

    ui adjustments

--- kibbeh/src/ui/SearchOverlay.tsx
+++ kibbeh/src/ui/SearchOverlay.tsx
@@ -8,7 +8,7 @@ export const SearchOverlay: React.FC<SearchOverlayProps> = ({ children }) => {
   return (
     <div
       style={{ borderRadius: "8px" }}
-      className="w-full p-2 bg-primary-800 border border-primary-700"
+      className="w-full px-2 pt-2 pb-5 bg-primary-800 border border-primary-700"
     >
       {children}
     </div>
