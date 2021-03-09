5771c00618c367cf5d95d4abeae411f7bb3de45f
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-03-09 14:07:00 +0200

    ui adjustments

--- kibbeh/src/ui/SettingsIcon.tsx
+++ kibbeh/src/ui/SettingsIcon.tsx
@@ -11,14 +11,16 @@ export const SettingsIcon: React.FC<SettingsIconProps> = ({
   icon,
   label,
   trailingIcon,
-  classes,
+  classes = "",
 }) => {
   return (
     <div
-      className={`w-full font-bold items-center py-2 px-3 cursor-pointer hover:bg-primary-700 ${classes}`}
+      className={`w-full items-center px-4 py-2 cursor-pointer hover:bg-primary-700 ${classes}`}
     >
       {icon}
-      <span className="ml-2 text-button flex-1 capitalize flex-1">{label}</span>
+      <span className="ml-2 font-medium text-primary-100 flex-1 capitalize flex-1">
+        {label}
+      </span>
       {trailingIcon && trailingIcon}
     </div>
   );
