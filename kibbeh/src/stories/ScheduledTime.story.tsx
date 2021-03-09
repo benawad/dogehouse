06bf384632a7d466b85792e8cbebf169e77230fc
Ben Awad <benawad97@gmail.com>
2021-03-08 18:08:28 -0600

    add active

--- kibbeh/src/stories/ScheduledTime.story.tsx
+++ kibbeh/src/stories/ScheduledTime.story.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Story } from "@storybook/react";
 import { ScheduledTime } from "../ui/ScheduledTime";
+import { toBoolean } from "./utils/toBoolean";
 
 export default {
   title: "ScheduledTime",
 };
 
-const TheScheduledTime: Story = ({ children }) => {
-  return <ScheduledTime>{children || `12:30 PM`}</ScheduledTime>;
+const TheScheduledTime: Story = ({ active, children }) => {
+  return (
+    <ScheduledTime active={active}>{children || `12:30 PM`}</ScheduledTime>
+  );
 };
 
 export const Main = TheScheduledTime.bind({});
+
+Main.argTypes = {
+  active: toBoolean(),
+};
--- kibbeh/src/ui/ScheduledTime.tsx
+++ kibbeh/src/ui/ScheduledTime.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const ScheduledTime: React.FC = ({ children }) => {
+export const ScheduledTime: React.FC<{ active?: boolean }> = ({
+  active,
+  children,
+}) => {
   return (
     <div className="text-primary-200 font-bold items-center">
-      <div className="inline-block mr-2 w-2 h-2 rounded-full bg-primary-300"></div>
+      <div
+        className={`inline-block mr-2 w-2 h-2 rounded-full ${
+          active ? "bg-accent" : "bg-primary-300"
+        }`}
+      ></div>
       {children}
     </div>
   );
