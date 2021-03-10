ce295c05d3026e1abff19bf6c747763ad4707880
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-03-09 23:44:31 -0800

    tests for rooms get_room_status and can_join_room

--- kousa/test/beef/rooms_test.exs
+++ kousa/test/beef/rooms_test.exs
@@ -4,8 +4,67 @@ defmodule Kousa.Beef.RoomsTest do
   use Kousa.Support.EctoSandbox
 
   alias Kousa.Support.Factory
+  alias Beef.Schemas.User
+  alias Beef.Schemas.Room
+  alias Beef.Rooms
 
   describe "Rooms" do
 
+    # need to mock user sessions
+    # not working atm
+    @tag :skip
+    test "get_room_status" do
+      user = Factory.create(User)
+      # if not in room
+      assert Rooms.get_room_status(user.id) == {nil, nil}
+
+      # if user is creator of room
+      # room = Factory.create(Room, %{creatorId: user.id})
+      # Rooms.join_room(room, user.id)
+      # assert Rooms.get_room_status(user.id) == {:creator, room}
+
+      # if user it not creator
+      # listener
+      # room = Factory.create(Room)
+      # Beef.Rooms.join_room(room, user.id)
+      # assert Rooms.get_room_status(user.id) == {:listener, room}
+
+      # # mod
+      # Beef.Data.RoomPermission.insert(%{ userId: user.id, roomId: room.id, isMod: true })
+      # assert Rooms.get_room_status(user.id) == {:mod, room}
+
+      # # speaker
+      # Beef.Data.RoomPermission.upsert(%{ userId: user.id, roomId: room.id }, %{ isMod: false, isSpeaker: true })
+      # assert Rooms.get_room_status(user.id) == {:speaker, room}
+
+      # # askedToSpeak
+      # Beef.Data.RoomPermission.upsert(%{ userId: user.id, roomId: room.id }, %{ isMod: false, isSpeaker: false, askedToSpeak: true })
+      # assert Rooms.get_room_status(user.id) == {:askedToSpeak, room}
+    end
+
+    test "can_join_room" do
+      u = Factory.create(User)
+
+      # non existent room
+      assert Beef.Rooms.can_join_room("f6834652-b876-4641-9639-c5d04d87dc96", u.id) == {:error, "room doesn't exist anymore"}
+
+      max_room_size = Application.fetch_env!(:kousa, :max_room_size)
+
+      room = Factory.create(Room, [{ :numPeopleInside, max_room_size }])
+      assert Beef.Rooms.can_join_room(room.id, u.id) == {:error, "room is full"}
+
+      room2 = Factory.create(Room)
+      Kousa.Data.RoomBlock.insert(%{ userId: u.id, roomId: room2.id, modId: u.id })
+      assert Beef.Rooms.can_join_room(room2.id, u.id) == {:error, "you are blocked from the room"}
+
+      creator = Factory.create(User)
+      room3 = Factory.create(Room, [{ :creatorId, creator.id }])
+      Beef.UserBlocks.insert(%{ userId: creator.id, userIdBlocked: u.id })
+      assert Beef.Rooms.can_join_room(room3.id, u.id) == {:error, "the creator of the room blocked you"}
+
+
+      room4 = Factory.create(Room)
+      assert Beef.Rooms.can_join_room(room4.id, u.id) == {:ok, room4}
+    end
   end
 end
