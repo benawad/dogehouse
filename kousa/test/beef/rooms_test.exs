77c91281ee26b32d1531b8698529c45ec25e1a67
arth3rs0ng@gmail.com <arthursong14@gmail.com>
2021-03-09 22:12:31 -0800

    create test file for rooms

---
+++ kousa/test/beef/rooms_test.exs
@@ -0,0 +1,11 @@
+defmodule Kousa.Beef.RoomsTest do
+  # allow tests to run in parallel
+  use ExUnit.Case, async: true
+  use Kousa.Support.EctoSandbox
+
+  alias Kousa.Support.Factory
+
+  describe "Rooms" do
+
+  end
+end
