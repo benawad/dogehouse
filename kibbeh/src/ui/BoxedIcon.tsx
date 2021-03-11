6546fb3f92fc793479abe7474f1e8268c9a494e7
Connor Hancovsky <chancovsky@gmail.com>
2021-03-11 11:43:56 -0800

    Update classNames and icons

--- kibbeh/src/ui/BoxedIcon.tsx
+++ kibbeh/src/ui/BoxedIcon.tsx
@@ -7,8 +7,7 @@ export const BoxedIcon: React.FC<HTMLAttributes<HTMLElement>> = ({
 }) => {
   return (
     <div
-      className="bg-primary-700 text-primary-100 justify-center items-center rounded"
-      style={{ height: "40px", width: "40px" }}
+      className="bg-primary-700 hover:bg-primary-600 h-6 w-6 cursor-pointer text-primary-100 justify-center items-center rounded"
       {...props}
     >
       {children}
