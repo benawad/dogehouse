ae1925648adb9cd64ec93bb2c809ff4ce64d7a96
ChVt <chaivat@tutanota.com>
2021-03-12 17:36:37 +0700

    Added THAI language

--- kofta/src/app/components/LanguageSelector.tsx
+++ kofta/src/app/components/LanguageSelector.tsx
@@ -16,6 +16,7 @@ export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
 		{ value: "nb", label: "nb" },
 		{ value: "pt-BR", label: "pt-br" },
 		{ value: "pt-PT", label: "pt-pt" },
+		{ value: "th", label: "th" },
 		{ value: "tr", label: "tr" },
 		{ value: "zh-CN", label: "zh-cn" }
 	],
