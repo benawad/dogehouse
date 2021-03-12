a9736a54963f601a8ad9e1554ba0ac465bd20c68
kitravee <kitravee.swk@gmail.com>
2021-03-13 01:35:34 +0700

    Updated ProfileButton Style

--- kofta/src/app/components/ProfileButton.tsx
+++ kofta/src/app/components/ProfileButton.tsx
@@ -4,19 +4,19 @@ import { useMeQuery } from "../utils/useMeQuery";
 import { Avatar } from "./Avatar";
 
 interface ProfileButtonProps {
-	size?: number;
-	circle?: boolean;
+  size?: number;
+  circle?: boolean;
 }
 
 export const ProfileButton: React.FC<ProfileButtonProps> = ({
-	size = 41,
-	circle,
+  size = 41,
+  circle,
 }) => {
-	const { me } = useMeQuery();
-	const history = useHistory();
-	return me ? (
-		<button onClick={() => history.push("/me")} className={`px-2.5`}>
-			<Avatar circle={circle} size={size} src={me.avatarUrl} />
-		</button>
-	) : null;
+  const { me } = useMeQuery();
+  const history = useHistory();
+  return me ? (
+    <button onClick={() => history.push("/me")}>
+      <Avatar circle={circle} size={size} src={me.avatarUrl} />
+    </button>
+  ) : null;
 };
