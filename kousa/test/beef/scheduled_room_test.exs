3fae4fe1a3d6e2a448749b520467cefb3163b6a0
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-03-13 10:54:32 -0800

    fixes scheduled rooms test

--- kousa/test/beef/scheduled_room_test.exs
+++ kousa/test/beef/scheduled_room_test.exs
@@ -1,4 +1,4 @@
-defmodule Kousa.Database.ScheduledRoomTest do
+defmodule Kousa.Beef.ScheduledRoomsTest do
   # allow tests to run in parallel
   use ExUnit.Case, async: true
   use Kousa.Support.EctoSandbox
@@ -7,6 +7,7 @@ defmodule Kousa.Database.ScheduledRoomTest do
   alias Beef.Repo
   alias Beef.Schemas.User
   alias Beef.Schemas.ScheduledRoom
+  alias Beef.ScheduledRooms
 
   defp create_user(_) do
     {:ok, user: Factory.create(User)}
