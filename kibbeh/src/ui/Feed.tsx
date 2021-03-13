1dbccf99a828d245a1357ac81cc0f3bf417f360c
Yves Bode <ynrbode@gmail.com>
2021-03-12 22:12:32 -0300

    Removed scheduledRooms and side effect of rooms prop

--- kibbeh/src/ui/Feed.tsx
+++ kibbeh/src/ui/Feed.tsx
@@ -1,12 +1,6 @@
-import React, {
-  MouseEventHandler,
-  useState,
-  useEffect,
-  ReactNode,
-} from "react";
+import React, { MouseEventHandler, ReactNode } from "react";
 import { RoomCardProps, RoomCard } from "./RoomCard";
 import { Button } from "./Button";
-import { isPast } from "date-fns";
 
 export interface FeedProps {
   title: string;
@@ -23,14 +17,6 @@ export const Feed: React.FC<FeedProps> = ({
   rooms,
   emptyPlaceholder,
 }) => {
-  const [liveRooms, setLiveRooms] = useState<RoomCardProps[]>([]);
-  const [scheduledRooms, setScheduledRooms] = useState<RoomCardProps[]>([]);
-
-  useEffect(() => {
-    setLiveRooms(rooms.filter((room) => isPast(room.scheduledFor)));
-    setScheduledRooms(rooms.filter((room) => !isPast(room.scheduledFor)));
-  }, [rooms]);
-
   return (
     <div className="flex-1 flex-col">
       <div className="justify-between items-end mb-5">
@@ -39,20 +25,8 @@ export const Feed: React.FC<FeedProps> = ({
       </div>
       <div className="flex-col space-y-4">
         {rooms.length === 0 && emptyPlaceholder}
-        {liveRooms.map((room, index) => (
-          <RoomCard
-            key={index}
-            title={room.title}
-            subtitle={room.subtitle}
-            scheduledFor={room.scheduledFor}
-            listeners={room.listeners}
-            tags={room.tags}
-          />
-        ))}
-        {liveRooms.length > 0 && scheduledRooms.length > 0 && (
-          <hr className="border-primary-600" />
-        )}
-        {scheduledRooms.map((room, index) => (
+        {/* <hr className="border-primary-600" /> */}
+        {rooms.map((room, index) => (
           <RoomCard
             key={index}
             title={room.title}
