dcea72a67dc9a487e0dd31b78135d7939847f120
Ben Awad <benawad97@gmail.com>
2021-03-16 11:01:43 -0500

    custom emotes

--- kofta/src/app/modules/room-chat/RoomChatInput.tsx
+++ kofta/src/app/modules/room-chat/RoomChatInput.tsx
@@ -11,6 +11,7 @@ import { Codicon } from "../../svgs/Codicon";
 import { createChatMessage } from "../../utils/createChatMessage";
 import { useMeQuery } from "../../utils/useMeQuery";
 import { useTypeSafeTranslation } from "../../utils/useTypeSafeTranslation";
+import { customEmojis } from "./EmoteData";
 import { useRoomChatMentionStore } from "./useRoomChatMentionStore";
 import { useRoomChatStore } from "./useRoomChatStore";
 
@@ -73,18 +74,6 @@ export const RoomChatInput: React.FC<ChatInputProps> = () => {
     }
   };
 
-  const addEmoji = (emoji: any) => {
-    position =
-      (position === 0 ? inputRef!.current!.selectionStart : position + 2) || 0;
-
-    const newMsg = [
-      message.slice(0, position),
-      emoji.native,
-      message.slice(position),
-    ].join("");
-    setMessage(newMsg);
-  };
-
   const handleSubmit = (
     e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
   ) => {
@@ -110,10 +99,10 @@ export const RoomChatInput: React.FC<ChatInputProps> = () => {
     }
 
     const tmp = message;
-    const messageData = createChatMessage(tmp, mentions, currentRoom?.users)
-    
+    const messageData = createChatMessage(tmp, mentions, currentRoom?.users);
+
     // dont empty the input, if no tokens
-    if (!messageData.tokens.length) return
+    if (!messageData.tokens.length) return;
     setMessage("");
 
     if (
@@ -141,7 +130,17 @@ export const RoomChatInput: React.FC<ChatInputProps> = () => {
         <Picker
           set="apple"
           onSelect={(emoji) => {
-            addEmoji(emoji);
+            position =
+              (position === 0
+                ? inputRef!.current!.selectionStart
+                : position + 2) || 0;
+
+            const newMsg = [
+              message.slice(0, position),
+              "native" in emoji ? emoji.native : emoji.colons,
+              message.slice(position),
+            ].join("");
+            setMessage(newMsg);
           }}
           style={{
             position: "relative",
@@ -155,6 +154,7 @@ export const RoomChatInput: React.FC<ChatInputProps> = () => {
           }}
           sheetSize={32}
           theme="dark"
+          custom={customEmojis}
           emojiTooltip={true}
           showPreview={false}
           showSkinTones={false}
