3e065294b42a18a660f5fcb472f6d4e17a52ce37
VayerMaking <martin.vayer@gmail.com>
2021-03-16 12:57:54 +0200

    added Bulgarian language selection

--- kofta/src/app/components/LanguageSelector.tsx
+++ kofta/src/app/components/LanguageSelector.tsx
@@ -41,6 +41,7 @@ export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
     { value: "tr", label: "🇹🇷 Turkish" },
     { value: "uk", label: "🇺🇦 Ukrainian" },
     { value: "ur", label: "🇵🇰 Urdu" },
+    { value: "bg", label: "🇧🇬 Bulgarian"},
   ],
 }) => {
   const { i18n } = useTranslation();
