3de375f1feec03bbd0ee7bce79c1932203fb1be4
abdi rahman hashi <logark100@gmail.com>
2021-03-16 22:34:01 +0300

    making userSearchpage  better

--- kofta/src/app/pages/SearchUsersPage.tsx
+++ kofta/src/app/pages/SearchUsersPage.tsx
@@ -7,6 +7,9 @@ import { Wrapper } from "../components/Wrapper";
 import { Codicon } from "../svgs/Codicon";
 import { BodyWrapper } from "../components/BodyWrapper";
 import { useTypeSafeTranslation } from "../utils/useTypeSafeTranslation";
+import "../../css/search.css";
+import { BottomVoiceControl } from "../components/BottomVoiceControl";
+import { ProfileButton } from "../components/ProfileButton";
 
 interface SearchUsersProps {}
 
@@ -15,29 +18,41 @@ export const SearchUsersPage: React.FC<SearchUsersProps> = ({}) => {
   const [query, setQuery] = useState("");
   const { t } = useTypeSafeTranslation();
   return (
-    <Wrapper>
-      <Backbar />
-      <BodyWrapper>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (query) {
-            }
-          }}
-          className={`flex`}
-        >
-          <input
-            autoFocus
-            placeholder={t("pages.searchUser.search")}
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <Button type="submit" variant="small">
-            <Codicon name="search" />
-          </Button>
-        </form>
-        {loading ? <div className={`my-8`}>{t("common.loading")}</div> : null}
-      </BodyWrapper>
-    </Wrapper>
+    <div className={`flex flex-col flex-1`}>
+      <Wrapper>
+        <Backbar>
+          <h1
+            className={`font-xl flex-1 text-center flex items-center justify-center text-2xl`}
+          >
+            Search
+            {/* {t("modules.scheduledRooms.title")} */}
+          </h1>
+          <ProfileButton />
+        </Backbar>
+        <BodyWrapper>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (query) {
+              }
+            }}
+            className={`flex`}
+          >
+            <input
+              className={`searchTerm`}
+              autoFocus
+              placeholder={t("pages.searchUser.search")}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <Button className={`searchButton`} type="submit" variant="small">
+              <Codicon name="search" />
+            </Button>
+          </form>
+          {loading ? <div className={`my-8`}>{t("common.loading")}</div> : null}
+        </BodyWrapper>
+      </Wrapper>
+      <BottomVoiceControl />
+    </div>
   );
 };
