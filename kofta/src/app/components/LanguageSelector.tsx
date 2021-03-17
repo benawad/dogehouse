88b8bbf1b6d2f3d49780fed0308d4d85a0ff41b0
MaySoMusician <maysomusician@gmail.com>
2021-03-17 23:45:29 +0900

    Add ISO language name as comments

--- kofta/src/app/components/LanguageSelector.tsx
+++ kofta/src/app/components/LanguageSelector.tsx
@@ -7,47 +7,47 @@ interface LanguageSelectorProps {
 
 export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
   options = [
-    { value: "en", label: "🇬🇧 English" },
-    { value: "af", label: "🇿🇦 Afrikaans" },
-    { value: "am", label: "🇪🇹 አማርኛ" },
-    { value: "ar", label: "🇸🇦 عربي" },
-    { value: "az", label: "🇦🇿 Azərbaycanca" },
-    { value: "bn", label: "🇧🇩 বাংলা" },
-    { value: "zh-CN", label: "🇨🇳 中文 (简体)" },
-    { value: "cs", label: "🇨🇿 Čeština" },
-    { value: "da", label: "🇩🇰 Dansk" },
-    { value: "nl", label: "🇳🇱 Nederlands" },
-    { value: "et", label: "🇪🇪 Eesti keel" },
-    { value: "fi", label: "🇫🇮 suomi" },
-    { value: "fr", label: "🇫🇷 Français" },
-    { value: "de", label: "🇩🇪 Deutsch" },
-    { value: "he", label: "🇮🇱 עברית" },
-    { value: "hi", label: "🇮🇳 हिन्दी" },
-    { value: "hu", label: "🇭🇺 magyar" },
-    { value: "is", label: "🇮🇸 Íslenska" },
-    { value: "id", label: "🇮🇩 Bahasa Indonesia" },
-    { value: "it", label: "🇮🇹 Italiano" },
-    { value: "ja", label: "🇯🇵 日本語" },
-    { value: "lt", label: "🇱🇹 Lietuvių" },
-    { value: "ne", label: "🇳🇵 नेपाली" },
-    { value: "nb", label: "🇳🇴 Norsk Bokmål" },
-    { value: "pl", label: "🇵🇱 Polski" },
-    { value: "pt-PT", label: "🇵🇹 Português (Europeu)" },
-    { value: "pt-BR", label: "🇧🇷 Português (do Brasil)" },
-    { value: "ro", label: "🇷🇴 Română" },
-    { value: "ru", label: "🇷🇺 Русский" },
-    { value: "sk", label: "🇸🇰 slovenčina" },
-    { value: "sq", label: "🇦🇱 Shqip" },
-    { value: "sr", label: "🇷🇸 Српски" },
-    { value: "sr-Latin", label: "🇷🇸 Srpski" },
-    { value: 'sv', label: "🇸🇪 Svenska" },
-    { value: "es", label: "🇪🇸 Español" },
-    { value: "th", label: "🇹🇭 ไทย" },
-    { value: "tr", label: "🇹🇷 Türkçe" },
-    { value: "uk", label: "🇺🇦 Українська" },
-    { value: "ur", label: "🇵🇰 اردو" },
+    { value: "en", label: "🇬🇧 English" }, // English
+    { value: "af", label: "🇿🇦 Afrikaans" }, // Afrikaans
+    { value: "am", label: "🇪🇹 አማርኛ" }, // Amharic
+    { value: "ar", label: "🇸🇦 عربي" }, // Arabic
+    { value: "az", label: "🇦🇿 Azərbaycanca" }, // Azerbaijani
+    { value: "bn", label: "🇧🇩 বাংলা" }, // Bengali
+    { value: "zh-CN", label: "🇨🇳 中文 (简体)" }, // Chinese (Simplified)
+    { value: "cs", label: "🇨🇿 Čeština" }, // Czech
+    { value: "da", label: "🇩🇰 Dansk" }, // Danish
+    { value: "nl", label: "🇳🇱 Nederlands" }, // Dutch
+    { value: "et", label: "🇪🇪 Eesti keel" }, // Estonian
+    { value: "fi", label: "🇫🇮 suomi" }, // Finnish
+    { value: "fr", label: "🇫🇷 Français" }, // French
+    { value: "de", label: "🇩🇪 Deutsch" }, // German
+    { value: "he", label: "🇮🇱 עברית" }, // Hebrew
+    { value: "hi", label: "🇮🇳 हिन्दी" }, // Hindi
+    { value: "hu", label: "🇭🇺 magyar" }, // Hungarian
+    { value: "is", label: "🇮🇸 Íslenska" }, // Icelandic
+    { value: "id", label: "🇮🇩 Bahasa Indonesia" }, // Indonesian
+    { value: "it", label: "🇮🇹 Italiano" }, // Italian
+    { value: "ja", label: "🇯🇵 日本語" }, // Japanese
+    { value: "lt", label: "🇱🇹 Lietuvių" }, // Lithuanian
+    { value: "ne", label: "🇳🇵 नेपाली" }, // Nepali
+    { value: "nb", label: "🇳🇴 Norsk Bokmål" }, // Norwegian Bokmål
+    { value: "pl", label: "🇵🇱 Polski" }, // Polish
+    { value: "pt-PT", label: "🇵🇹 Português (Europeu)" }, // Portuguese (Portugal)
+    { value: "pt-BR", label: "🇧🇷 Português (do Brasil)" }, // Portuguese (Brazil)
+    { value: "ro", label: "🇷🇴 Română" }, // Romanian
+    { value: "ru", label: "🇷🇺 Русский" }, // Russian
+    { value: "sk", label: "🇸🇰 slovenčina" }, // Slovak
+    { value: "sq", label: "🇦🇱 Shqip" }, // Albanian
+    { value: "sr", label: "🇷🇸 Српски" }, // Serbian
+    { value: "sr-Latin", label: "🇷🇸 Srpski" }, // Serbian (Latin)
+    { value: 'sv', label: "🇸🇪 Svenska" }, // Swedish
+    { value: "es", label: "🇪🇸 Español" }, // Estonian
+    { value: "th", label: "🇹🇭 ไทย" }, // Thai
+    { value: "tr", label: "🇹🇷 Türkçe" }, // Turkish
+    { value: "uk", label: "🇺🇦 Українська" }, // Ukrainian
+    { value: "ur", label: "🇵🇰 اردو" }, // Urdu
     { value: "owo", label: "OwO english"},
-    { value: "bg", label: "🇧🇬 Български"},
+    { value: "bg", label: "🇧🇬 Български"}, // Bulgarian
 		{ value: "en-pirate", label: "🏴‍☠️ Pirate"},
   ],
 }) => {
