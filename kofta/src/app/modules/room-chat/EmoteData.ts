4e1eca5794804e9e5351ddd5ab954630ba43f777
AndreasHGK <36563463+AndreasHGK@users.noreply.github.com>
2021-03-18 11:52:51 +0100

    feat(kofta): add thonk and catJAM emote

--- kofta/src/app/modules/room-chat/EmoteData.ts
+++ kofta/src/app/modules/room-chat/EmoteData.ts
@@ -19,6 +19,18 @@ export const customEmojis = [
     keywords: ["peped", "pepe"],
     imageUrl: "/emotes/peped.gif",
   },
+	{
+		name: "catJAM",
+		short_names: ["catJAM"],
+		keywords: ["catjam", "vibe"],
+		imageUrl: "/emotes/catjam.gif",
+	},
+	{
+		name: "Thonk",
+		short_names: ["Thonk"],
+		keywords: ["thonk", "think"],
+		imageUrl: "/emotes/thonk.png",
+	},
 ].map((e) => ({ ...e, customCategory: "BetterTTV", text: "", emoticons: [] }));
 
 export const emoteMap: Record<string, string> = {};
