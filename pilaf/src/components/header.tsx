6f50f75d41360ffda0b16ff44f1081393255d3dc
Etienne <etienne@mapwize.io>
2021-03-18 16:21:20 +0100

    fix(pilaf): adding padding top on header to improve Android UI

--- pilaf/src/components/header.tsx
+++ pilaf/src/components/header.tsx
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.primary900,
     flexDirection: "row",
     padding: 16,
-    paddingTop: 0,
+    paddingTop: 16,
   },
   leftContainer: {},
   rightContainer: {
