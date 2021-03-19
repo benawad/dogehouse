8118a92496ee63c197a893f0945891ba3b1120e6
Etienne <etienne@mapwize.io>
2021-03-19 11:55:56 +0100

    feat(pilaf): add UI Components

---
+++ pilaf/src/pages/NotificationsPage.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FollowNotification } from "../components/notifications/FollowNotification";
+import { LiveNotification } from "../components/notifications/LiveNotification";
+import { NewRoomNotification } from "../components/notifications/NewRoomNotification";
+import { colors, fontFamily } from "../constants/dogeStyle";
+
+export const NotificationsPage: React.FC = () => {
+  return (
+    <SafeAreaView
+      style={styles.safeAreaView}
+      edges={["bottom", "left", "right"]}
+    >
+      <ScrollView>
+        <NewRoomNotification
+          username={"DrMadTurkey"}
+          time={"now"}
+          joined={true}
+        />
+        <LiveNotification username={"DrMadTurkey"} time={"now"} joined={true} />
+        <FollowNotification
+          username={"DrMadTurkey"}
+          userAvatarSrc={require("../assets/images/100.png")}
+          time={"now"}
+          isOnline={true}
+          following={true}
+        />
+        <FollowNotification
+          username={"DrMadTurkey"}
+          userAvatarSrc={require("../assets/images/100.png")}
+          time={"now"}
+          isOnline={true}
+          following={true}
+        />
+        <FollowNotification
+          username={"DrMadTurkey"}
+          userAvatarSrc={require("../assets/images/100.png")}
+          time={"now"}
+          isOnline={true}
+          following={true}
+        />
+        <FollowNotification
+          username={"DrMadTurkey"}
+          userAvatarSrc={require("../assets/images/100.png")}
+          time={"now"}
+          isOnline={true}
+          following={true}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: colors.primary800,
+  },
+});
