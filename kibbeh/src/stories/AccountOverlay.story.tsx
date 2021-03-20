1de6db748634269e8700fa5a92098de646c503aa
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-03-20 21:01:41 +0200

    feat(kibbeh): make AccountOverlay draggable

--- kibbeh/src/stories/AccountOverlay.story.tsx
+++ kibbeh/src/stories/AccountOverlay.story.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Story } from "@storybook/react";
+import { DraggableEventHandler } from "react-draggable";
 
 import {
   AccountOverlay,
@@ -10,14 +11,42 @@ export default {
   title: "AccountOverlay",
   component: AccountOverlay,
 };
+const defaultProps: AccountOverlyProps = {
+  axis: "y",
+  position: { x: 0, y: 0 },
+  bounds: { top: -363, bottom: 0 },
+  children: <div style={{ color: "white" }}>Use the handle to drag me up!</div>,
+  dragHandler: (_e, ui) => console.log(ui.deltaY),
+};
 
-export const Main: Story<AccountOverlyProps> = ({
-  children = "",
-  className = "",
-}) => (
-  <div style={{ width: "414px", height: "362px" }}>
-    <AccountOverlay className={className}>{children}</AccountOverlay>
-  </div>
-);
+export const Main: Story<AccountOverlyProps> = (props) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const dragHandler: DraggableEventHandler = (_e, ui) =>
+    setPosition({ x: 0, y: ui.y });
+
+  return (
+    <div
+      style={{
+        width: "414px",
+        height: "362x",
+        border: "1px solid #fd6868",
+        position: "relative",
+        top: "500px",
+      }}
+    >
+      <div style={{ width: "414px", height: "362px" }}>
+        <AccountOverlay
+          {...props}
+          axis={props.axis || defaultProps.axis}
+          bounds={props.bounds || defaultProps.bounds}
+          position={props.position || position}
+          dragHandler={props.dragHandler || dragHandler}
+        >
+          {props.children || defaultProps.children}
+        </AccountOverlay>
+      </div>
+    </div>
+  );
+};
 
 Main.bind({});
--- kibbeh/src/ui/mobile/AccountOverlay.tsx
+++ kibbeh/src/ui/mobile/AccountOverlay.tsx
@@ -1,24 +1,47 @@
 import React, { ReactNode } from "react";
+import Draggable, {
+  DraggableBounds,
+  ControlPosition,
+  DraggableEventHandler,
+} from "react-draggable";
 
 export interface AccountOverlyProps {
   children: ReactNode;
+  position: ControlPosition;
+  dragHandler: DraggableEventHandler;
+  bounds: DraggableBounds;
   className?: string;
+  axis?: "both" | "x" | "y" | "none" | undefined;
 }
 
 export const AccountOverlay: React.FC<AccountOverlyProps> = ({
   children,
+  position,
+  bounds,
+  dragHandler,
   className = "",
+  axis = "both",
 }) => {
   return (
-    <div
-      style={{ padding: "43px 25px" }}
-      className={`w-full rounded-t-8 px-2 pt-7 pb-5 bg-primary-800 relative  ${className}`}
-      data-testid="account-overlay"
+    <Draggable
+      onDrag={dragHandler}
+      axis={axis}
+      handle="#handle"
+      bounds={bounds}
+      position={position}
     >
-      <>
-        <span className="absolute bg-primary-300 rounded-8 inline-block top-3 left-1 right-1 w-6 h-1 m-auto"></span>
+      <div
+        style={{ padding: "43px 25px" }}
+        className={`w-full rounded-t-8 px-2 pt-7 pb-5 bg-primary-800 relative  ${className}`}
+        data-testid="account-overlay"
+      >
+        <span
+          id="handle"
+          style={{ cursor: "grab" }}
+          className="absolute bg-primary-300 rounded-8 inline-block top-3 left-1 right-1 w-6 h-1 m-auto"
+        ></span>
         {children}
-      </>
-    </div>
+      </div>
+    </Draggable>
   );
 };
