a30e19cb258cf86af2a2160754fca88cecd7a418
GoldyyDev <55450464+goldyydev@users.noreply.github.com>
2021-03-20 02:57:25 +0200

    Optimized the SearchBar for mobile

--- kibbeh/src/ui/Search/SearchBar.tsx
+++ kibbeh/src/ui/Search/SearchBar.tsx
@@ -5,20 +5,22 @@ import { Input } from "../Input";
 export interface SearchBarProps
   extends React.ComponentPropsWithoutRef<"input"> {
   inputClassName?: string;
+  mobile?: boolean;
 }
 
 export const SearchBar: React.FC<SearchBarProps> = ({
   className = "",
   inputClassName = "",
+  mobile = false,
   ...props
 }) => {
   return (
     <div
-      className={`w-full bg-primary-700 text-primary-300 focus-within:text-primary-100 rounded-lg ${className}`}
+      className={`w-full bg-primary-700 text-primary-300 focus-within:text-primary-100 rounded-lg ${mobile ? 'px-4' : ''} ${className}`}
     >
-      <div className="h-full mx-4 flex items-center pointer-events-none">
+      {!mobile && <div className="h-full mx-4 flex items-center pointer-events-none">
         <SmSolidSearch />
-      </div>
+      </div>}
       <Input className={inputClassName} {...props} />
     </div>
   );
