6a49e097d095cf66a906c60df5753bb807660d8b
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-03-20 16:01:22 +0200

    test(kibbeh): adds unit tests for AccountOverlay component

--- kibbeh/src/ui/mobile/AccountOverlay.tsx
+++ kibbeh/src/ui/mobile/AccountOverlay.tsx
@@ -13,6 +13,7 @@ export const AccountOverlay: React.FC<AccountOverlyProps> = ({
     <div
       style={{ padding: "43px 25px" }}
       className={`w-full rounded-t-8 px-2 pt-7 pb-5 bg-primary-800 relative  ${className}`}
+      data-testid="account-overlay"
     >
       <>
         <span className="absolute bg-primary-300 rounded-8 inline-block top-3 left-1 right-1 w-6 h-1 m-auto"></span>
