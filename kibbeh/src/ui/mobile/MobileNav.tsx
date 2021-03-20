be421463d3fdd4c80fe0b7a407cb43a1770b393d
tperm94 <67191889+tperm94@users.noreply.github.com>
2021-03-20 22:06:46 +0530

    update icons

--- kibbeh/src/ui/mobile/MobileNav.tsx
+++ kibbeh/src/ui/mobile/MobileNav.tsx
@@ -36,7 +36,9 @@ export const MobileNavItem: React.FC<MobileNavItemProps> = ({
         {children &&
           React.Children.map(children, (child) => {
             return React.cloneElement(child as React.ReactElement, {
-              className: isActive ? "text-accent h-4 w-4" : "text-primary-100 h-4 w-4",
+              className: isActive
+                ? "text-accent h-4 w-4"
+                : "text-primary-100 h-4 w-4",
             });
           })}
       </div>
