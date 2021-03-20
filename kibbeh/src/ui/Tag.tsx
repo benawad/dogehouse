4ed5588c07243769d9664fc1a8bc25ea364b8f3c
antriksh123 <antariksha.verma@pathways.in>
2021-03-20 20:10:37 +0530

    make changes to tag component

--- kibbeh/src/ui/Tag.tsx
+++ kibbeh/src/ui/Tag.tsx
@@ -6,7 +6,7 @@ export const Tag: React.FC<TagProps> = ({ children }) => {
   return (
     <div
       className={`cursor-pointer bg-primary-700 hover:bg-primary-600 text-xs px-2 font-bold text-primary-100 justify-center items-center rounded`}
-      style={{ height: "22px" }}
+      style={{ height: "22px", boxShadow: "0px 0px 7px rgba(253, 77, 77, 0.3)", borderColor: "#FD4D4D", borderStyle: "solid", borderWidth: 0.2 }}
     >
       {children}
     </div>
