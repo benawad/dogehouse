5ebc8774fb862a59bb656935b25c80c14957d4bd
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-03-20 02:41:35 +0200

    test(kibbeh): add unit tests for Input components

--- kibbeh/src/ui/Input.tsx
+++ kibbeh/src/ui/Input.tsx
@@ -9,9 +9,14 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     const cn = `w-full py-2 pr-4 bg-transparent text-primary-100 placeholder-primary-300 focus:outline-none ${className}`;
 
     return textarea ? (
-      <textarea ref={ref as any} className={cn} {...(props as any)} />
+      <textarea
+        ref={ref as any}
+        className={cn}
+        {...(props as any)}
+        data-testid="textarea"
+      />
     ) : (
-      <input ref={ref} className={cn} {...props} />
+      <input ref={ref} className={cn} {...props} data-testid="input" />
     );
   }
 );
