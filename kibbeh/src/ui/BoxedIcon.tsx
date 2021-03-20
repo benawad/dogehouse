3880f563916b0b6244e6233a3b653c8e3b10e263
Daksh <dakshshrivastava2007@gmail.com>
2021-03-20 16:28:39 +0530

    iscircular prop made

--- kibbeh/src/ui/BoxedIcon.tsx
+++ kibbeh/src/ui/BoxedIcon.tsx
@@ -1,19 +1,32 @@
 // React
-import React, { HTMLAttributes } from "react";
+import React, { ReactElement } from "react";
 
-export const BoxedIcon: React.FC<HTMLAttributes<HTMLElement>> = ({
+export interface BoxedIconProps {
+  classes ?: string;
+  children ?: ReactElement;
+  iscircular ?: boolean;
+}
+
+export const BoxedIcon: React.FC<BoxedIconProps> = ({
   children,
-  className = "",
-  ...props
+  classes = "",
+  iscircular = false,
 }) => {
   return (
-    <div
+    <div>
+    {iscircular ? (<div
       className={`bg-primary-700 hover:bg-primary-600 h-6 w-6 cursor-pointer text-primary-100 justify-center items-center rounded-full
-        ${className}`}
+        ${classes}`}
       data-testid="boxed-icon"
-      {...props}
     >
       {children}
-    </div>
+    </div>) : (<div
+      className={`bg-primary-700 hover:bg-primary-600 h-6 w-6 cursor-pointer text-primary-100 justify-center items-center rounded
+        ${classes}`}
+      data-testid="boxed-icon"
+    >
+      {children}
+    </div>)}
+     </div>
   );
 };
