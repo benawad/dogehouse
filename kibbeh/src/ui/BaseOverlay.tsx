55774a6c062e69c1b427110bd3451ec8f32e704f
AadityaMunjal <aadityamunjal06@gmail.com>
2021-03-20 19:52:43 +0530

    fix(kibbeh/src/ui/baseoverlay.tsx): remove outline from base overlays when focused

--- kibbeh/src/ui/BaseOverlay.tsx
+++ kibbeh/src/ui/BaseOverlay.tsx
@@ -28,7 +28,7 @@ export const BaseOverlay: React.FC<BaseOverlayProps> = ({
 
       {actionButton && (
         <button
-          className="px-4 bg-primary-700 text-primary-100 outline-none font-bold"
+          className="px-4 bg-primary-700 text-primary-100 focus:outline-none font-bold"
           style={{
             paddingTop: 8,
             paddingBottom: 12,
