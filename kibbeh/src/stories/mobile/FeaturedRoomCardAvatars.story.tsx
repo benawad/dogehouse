dabd49152ef5081c73e97055f33956ad7a931e1e
Ben Awad <benawad97@gmail.com>
2021-03-21 16:06:12 -0500

    prettier

--- kibbeh/src/stories/mobile/FeaturedRoomCardAvatars.story.tsx
+++ kibbeh/src/stories/mobile/FeaturedRoomCardAvatars.story.tsx
@@ -1,24 +1,23 @@
 import { Story } from "@storybook/react";
 import React from "react";
-import { FeaturedRoomCardAvatars, FeaturedRoomCardAvatarsProps } from '../../ui/mobile/FeaturedRoomCardAvatars'
+import {
+  FeaturedRoomCardAvatars,
+  FeaturedRoomCardAvatarsProps,
+} from "../../ui/mobile/FeaturedRoomCardAvatars";
 import src from "../../img/avatar.png";
 
 export default {
-  title: 'FeaturedRoomCardAvatars',
-  component: FeaturedRoomCardAvatars
+  title: "FeaturedRoomCardAvatars",
+  component: FeaturedRoomCardAvatars,
 };
 
 const Avatars: Story<FeaturedRoomCardAvatarsProps> = () => {
-  return (
-    <FeaturedRoomCardAvatars avatars={[src, src]} />
-  );
+  return <FeaturedRoomCardAvatars avatars={[src, src]} />;
 };
 
 const One: Story<FeaturedRoomCardAvatarsProps> = () => {
-  return (
-    <FeaturedRoomCardAvatars avatars={[src]} />
-  );
-}
+  return <FeaturedRoomCardAvatars avatars={[src]} />;
+};
 
 export const Main = Avatars.bind({});
 export const OneAvatar = One.bind({});
