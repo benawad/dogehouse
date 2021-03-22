794eba762f88a7e1337148ac13783b9db1341113
Ben Awad <benawad97@gmail.com>
2021-03-21 20:56:50 -0500

    fix tailwind config and room avatar

--- kibbeh/tailwind.config.js
+++ kibbeh/tailwind.config.js
@@ -17,6 +17,17 @@ module.exports = {
     },
     fontSize: {
       tiny: "0.625rem",
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
     },
     colors: {
       button: "var(--color-button-text)",
