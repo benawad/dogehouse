ba7fedaf628db6ca593315759200c8dfa39d622c
amitojsingh366 <amitojsingh366@gmail.com>
2021-03-24 10:50:04 +0300

    fixed conflicts again

--- baklava/src/utils/keybinds.ts
+++ baklava/src/utils/keybinds.ts
@@ -11,10 +11,6 @@ import {
     OVERLAY_KEY,
     isMac,
 } from "../constants";
-<<<<<<< HEAD
-=======
-import globkey from "globkey"
->>>>>>> 81899fd773b69809e0721df3b0275b7ac38b1453
 import { overlayWindow } from "electron-overlay-window";
 import { createOverlay } from "./overlay";
 import { startIPCHandler } from "./ipc";
@@ -26,12 +22,8 @@ export let CURRENT_INVITE_KEY = "Control+7";
 export let CURRENT_MUTE_KEY = "Control+m";
 export let CURRENT_CHAT_KEY = "Control+9";
 export let CURRENT_OVERLAY_KEY = "Control+Tab";
-<<<<<<< HEAD
 export let CURRENT_PTT_KEY = ["0", "Control"];
 export let CURRENT_PTT_KEY_STRING = "0,control"
-=======
-export let CURRENT_PTT_KEY = ['Key0','LControl'];
->>>>>>> 81899fd773b69809e0721df3b0275b7ac38b1453
 
 export let CURRENT_APP_TITLE = "";
 
@@ -113,7 +105,6 @@ export function RegisterKeybinds(bWindows: bWindowsType) {
 
     ipcMain.on("@overlay/app_title", (event, appTitle: string) => {
         CURRENT_APP_TITLE = appTitle;
-<<<<<<< HEAD
     })
     hook.raw((keypair: string[]) => {
         keypair.forEach(key => {
@@ -130,19 +121,5 @@ export function RegisterKeybinds(bWindows: bWindowsType) {
             PREV_PTT_STATUS = PTT;
         }
     })
-=======
-    });
-    globkey.raw(function(keypair) {
-        if (JSON.stringify(keypair) === JSON.stringify(['Key0','LControl'])) {
-            push_to_talk = true
-            console.log("push on")
-            bWindows.main.webContents.send("@voice/ptt_status_change", false);
-        } else if (push_to_talk) {
-            push_to_talk = false
-            console.log("push off")
-            bWindows.main.webContents.send("@voice/ptt_status_change", true);
-        }
-    });
->>>>>>> 81899fd773b69809e0721df3b0275b7ac38b1453
 }
 
