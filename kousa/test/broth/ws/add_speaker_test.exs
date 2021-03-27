677d017e2dd1b414949e2e298177108c82cea9ee
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-03-27 10:17:35 -0700

    adds tests on add_speaker

---
+++ kousa/test/broth/ws/add_speaker_test.exs
@@ -0,0 +1,51 @@
+defmodule KousaTest.Broth.Ws.BlockFromRoomTest do
+  use ExUnit.Case, async: true
+  use Kousa.Support.EctoSandbox
+
+  alias Beef.Schemas.User
+  alias Beef.Users
+  alias Broth.WsClient
+  alias Broth.WsClientFactory
+  alias Kousa.Support.Factory
+
+  require WsClient
+
+  setup do
+    user = Factory.create(User)
+    ws_client = WsClientFactory.create_client_for(user)
+
+    {:ok, user: user, ws_client: ws_client}
+  end
+
+  describe "the websocket add_speaker operation" do
+    test "makes the person a speaker", t do
+      # first, create a room owned by the primary user.
+      {:ok, %{room: %{id: room_id}}} = Kousa.Room.create_room(t.user.id, "foo room", "foo", false)
+      # make sure the user is in there.
+      assert %{currentRoomId: ^room_id} = Users.get_by_id(t.user.id)
+
+      # create a user that is logged in.
+      speaker = %{id: speaker_id} = Factory.create(User)
+      WsClientFactory.create_client_for(speaker)
+
+      # join the speaker user into the room
+      Kousa.Room.join_room(speaker_id, room_id)
+
+      WsClient.assert_frame("new_user_join_room", %{"user" => %{"id" => ^speaker_id}})
+
+      # block the person.
+      WsClient.send_msg(t.ws_client, "add_speaker", %{"userId" => speaker_id})
+
+      # both clients get notified
+      WsClient.assert_frame(
+        "speaker_added",
+        %{"userId" => ^speaker_id, "roomId" => ^room_id})
+
+      WsClient.assert_frame(
+        "speaker_added",
+        %{"userId" => ^speaker_id, "roomId" => ^room_id})
+
+      assert Beef.RoomPermissions.speaker?(speaker_id, room_id)
+    end
+  end
+end
