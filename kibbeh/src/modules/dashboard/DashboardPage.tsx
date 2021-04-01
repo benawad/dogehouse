19220e7e87348b2ca9650b47a1a071d9130826e9
GoldyyDev <goldyydev@pm.me>
2021-04-02 01:19:41 +0300

    Began the Header handler

--- kibbeh/src/modules/dashboard/DashboardPage.tsx
+++ kibbeh/src/modules/dashboard/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from 'next/head';
+import Header from "next/head";
 import { PageComponent } from "../../types/PageComponent";
 import { WaitForWsAndAuth } from "../auth/WaitForWsAndAuth";
 import { DesktopLayout } from "../layouts/DesktopLayout";
@@ -7,14 +7,14 @@ import { LeftPanel, MiddlePanel, RightPanel } from "../layouts/GridPanels";
 import { FeedController } from "./FeedController";
 import { FollowingOnlineController } from "./FollowingOnlineController";
 import { ProfileBlockController } from "./ProfileBlockController";
-import { HeaderController } from '../display/HeaderController';
+import { HeaderController } from "../display/HeaderController";
 
 interface LoungePageProps {}
 
 export const DashboardPage: PageComponent<LoungePageProps> = ({}) => {
   return (
     <WaitForWsAndAuth>
-      <HeaderController embed={{}}/>
+      <HeaderController embed={{}} />
       <DesktopLayout>
         <LeftPanel>
           <FollowingOnlineController />
--- kibbeh/src/modules/display/HeaderController.tsx
+++ kibbeh/src/modules/display/HeaderController.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from 'next/head';
+import Header from "next/head";
 
 export interface HeaderControllerProps {
-  title?: string,
-  embed?: {hexColor?: string, image?: string},
-  owner?: string,
-  additionalKeywords?: string[],
-  description?: string,
+  title?: string;
+  embed?: { hexColor?: string; image?: string };
+  owner?: string;
+  additionalKeywords?: string[];
+  description?: string;
 }
 
 export const HeaderController: React.FC<HeaderControllerProps> = ({
@@ -14,22 +14,30 @@ export const HeaderController: React.FC<HeaderControllerProps> = ({
   description = "Dogehouse is taking voice conversations to the moon 🚀",
   owner,
   additionalKeywords,
-  embed
+  embed,
 }) => {
   return (
-   <Header>
-    {title ? <title>{title} | DogeHouse</title> : <title>DogeHouse</title>}
-    <meta name="description" content={description}/>
-    {owner ? <meta name="author" content={owner}/> : ""}
-    <meta name="keywords" content={`DogeHouse, Doge${additionalKeywords?.map(k => `, ${k}`)}`}/>
-    <meta name="theme-color" content={embed?.hexColor || "#EFE7DD"}/>
-    {embed
-    ? <>
-        <meta name="og:title" content={description}/>
-        <meta name="og:type" content={owner ? "music.radio_station" : "website"}/>
-        {owner ? <meta name="music:creator" content={owner}/> : ""}
-      </>
-    : ""}
-  </Header>
+    <Header>
+      {title ? <title>{title} | DogeHouse</title> : <title>DogeHouse</title>}
+      <meta name="description" content={description} />
+      {owner ? <meta name="author" content={owner} /> : ""}
+      <meta
+        name="keywords"
+        content={`DogeHouse, Doge${additionalKeywords?.map((k) => `, ${k}`)}`}
+      />
+      <meta name="theme-color" content={embed?.hexColor || "#EFE7DD"} />
+      {embed ? (
+        <>
+          <meta name="og:title" content={description} />
+          <meta
+            name="og:type"
+            content={owner ? "music.radio_station" : "website"}
+          />
+          {owner ? <meta name="music:creator" content={owner} /> : ""}
+        </>
+      ) : (
+        ""
+      )}
+    </Header>
   );
 };
