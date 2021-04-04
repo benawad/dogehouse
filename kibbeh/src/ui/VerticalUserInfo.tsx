a26ef4f6e68ef93f97fbdf7c14617bae43541b75
Arthurdw <mail@arthurdw.com>
2021-04-04 18:10:10 +0200

    Fix for #1761

--- kibbeh/src/ui/VerticalUserInfo.tsx
+++ kibbeh/src/ui/VerticalUserInfo.tsx
@@ -14,44 +14,44 @@ interface VerticalUserInfoProps {
 export const VerticalUserInfo: React.FC<VerticalUserInfoProps> = ({ user }) => {
   const { t } = useTypeSafeTranslation();
   return (
-    <div className="flex-col rounded-8 pt-5 px-6 pb-4 w-full items-center">
+    <div className="flex-col items-center w-full px-6 pt-5 pb-4 rounded-8">
       <SingleUser
         size="default"
         src={user.avatarUrl}
         username={user.username}
       />
       <div className="mt-2">
-        <span className="text-primary-100 font-bold h-full break-all line-clamp-1">
+        <span className="h-full font-bold break-all text-primary-100 line-clamp-1">
           {user.displayName}
         </span>
-        <span className="text-primary-300 ml-1">@{user.username}</span>
+        <span className="ml-1 text-primary-300">@{user.username}</span>
         {/* <Badges badges={badges} /> */}
       </div>
       <div className="mt-2">
         <div>
           <ApiPreloadLink route="following" data={{ username: user.username }}>
-            <span className="text-primary-100 font-bold">
-              {kFormatter(user.numFollowers)}
+            <span className="font-bold text-primary-100">
+              {kFormatter(user.numFollowing)}
             </span>{" "}
-            <span className="text-primary-300 ml-1">
-              {t("pages.viewUser.followers")}
+            <span className="ml-1 text-primary-300">
+              {t("pages.viewUser.following")}
             </span>
           </ApiPreloadLink>
         </div>
         <div className="ml-4">
           <ApiPreloadLink route="followers" data={{ username: user.username }}>
-            <span className="text-primary-100 font-bold">
-              {kFormatter(user.numFollowing)}
+            <span className="font-bold text-primary-100">
+              {kFormatter(user.numFollowers)}
             </span>
-            <span className="text-primary-300 ml-1">
+            <span className="ml-1 text-primary-300">
               {" "}
-              {t("pages.viewUser.following")}
+              {t("pages.viewUser.followers")}
             </span>
           </ApiPreloadLink>
         </div>
       </div>
       <div className="w-full mt-2">
-        <p className="text-primary-300 mt-2 text-center w-full whitespace-pre-wrap break-words inline">
+        <p className="inline w-full mt-2 text-center break-words whitespace-pre-wrap text-primary-300">
           {user.bio.split(/\n/).map((line, i) => (
             <span key={i} className="flex justify-center">
               {line.split(" ").map((chunk, j) => {
@@ -60,7 +60,7 @@ export const VerticalUserInfo: React.FC<VerticalUserInfoProps> = ({ user }) => {
                     <a
                       href={normalizeUrl(chunk)}
                       rel="noreferrer"
-                      className="text-accent text-center hover:underline inline"
+                      className="inline text-center text-accent hover:underline"
                       key={`${i}${j}`}
                       target="_blank"
                     >
