a07ee1b5fdfe6af3c81f09028356c14a70ffcb5e
Isaac Mills <rooster0055@protonmail.com>
2021-04-04 20:46:51 -0400

    Update globkey for new, faster approach

--- baklava/src/utils/keybinds/worker.ts
+++ baklava/src/utils/keybinds/worker.ts
@@ -4,12 +4,10 @@ import globkey from 'globkey';
 if (!isMainThread) {
     let prev_keys = ['', '']
     let shouldBreak = false;
+    globkey.start();
     while (true) {
         if (shouldBreak) break;
         let keys = globkey.getKeys();
-        if (keys != prev_keys) {
-            if (parentPort) parentPort.postMessage({ type: 'keys', keys: keys });
-        }
-        prev_keys = keys
+        if (parentPort) parentPort.postMessage({ type: 'keys', keys: keys });
     }
 }
