a1ee6782f41c52a7f6b51839c6f37d5676b20013
load1n9 <prankboxofficials@gmail.com>
2021-04-05 13:39:00 -0400

    deno emote

--- kofta/src/app/modules/room-chat/EmoteData.ts
+++ kofta/src/app/modules/room-chat/EmoteData.ts
@@ -1592,6 +1592,12 @@ export const customEmojis = [
     keywords: ["cat", "dance", "party", "fun"],
     imageUrl: "/emotes/catDance.gif",
   },
+  {
+    name: "deno",
+    short_names: ["deno"],
+    keywords: ["deno", "denosaur"],
+    imageUrl: "/emotes/deno.png",
+  },
 ].map((e) => ({ ...e, customCategory: "Custom", text: "", emoticons: [] }));
 
 export const emoteMap: Record<string, string> = {};
