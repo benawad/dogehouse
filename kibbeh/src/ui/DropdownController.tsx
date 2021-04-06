21891786d4e2079c0f243db0646a378a81aafed8
Ben Awad <benawad97@gmail.com>
2021-04-05 20:43:54 -0500

    performance

--- kibbeh/src/ui/DropdownController.tsx
+++ kibbeh/src/ui/DropdownController.tsx
@@ -14,6 +14,7 @@ export const DropdownController: React.FC<{
     referenceRef.current,
     popperRef.current,
     {
+      modifiers: [{ name: "eventListeners", enabled: visible }],
       placement: "left",
     }
   );
