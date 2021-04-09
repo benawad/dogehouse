5f272b8d42bd752798de76c822dae52d4635caa4
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-10 01:46:26 +0300

    add disc client id env var

--- baklava/src/utils/rpc/index.ts
+++ baklava/src/utils/rpc/index.ts
@@ -3,10 +3,12 @@ import { Client, Presence } from 'discord-rpc';
 import logger from 'electron-log';
 import { startRPCIPCHandler, stopRPCIPCHandler } from './ipc';
 import dotenv from 'dotenv';
+// @ts-ignore
+import { DISCORD_CLIENT_ID } from '../../constants';
 
 dotenv.config();
 
-const clientId = process.env.DISCORD_CLIENT_ID || '';
+const clientId = DISCORD_CLIENT_ID || process.env.DISCORD_CLIENT_ID;
 let client: Client;
 
 export let RPC_RUNNING = false;
