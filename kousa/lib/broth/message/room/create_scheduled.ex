15a12df914ada877878fe4277bc410078748c743
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-04-12 18:10:20 -0700

    resolves create_scheduled

--- kousa/lib/broth/message/room/create_scheduled.ex
+++ kousa/lib/broth/message/room/create_scheduled.ex
@@ -4,23 +4,32 @@ defmodule Broth.Message.Room.CreateScheduled do
     operation: "room:create_scheduled:reply"
 
   schema "scheduled_room" do
-    field(:room, :string)
+    field(:name, :string)
+    field(:scheduledFor, :utc_datetime_usec)
+    field(:description, :string, default: "")
+
+    belongs_to(:creator, User, foreign_key: :creatorId, type: :binary_id)
+  end
+
+  @impl true
+  def changeset(initializer \\ %__MODULE__{}, data) do
+    initializer
+    |> cast(data, [:name, :scheduledFor, :description])
+    |> validate_required([:name, :scheduledFor])
+    |> validate_future
+  end
+
+  def validate_future(changeset = %{valid?: false}), do: changeset
+  def validate_future(changeset) do
+    changeset
+    |> get_field(:scheduledFor)
+    |> DateTime.compare(DateTime.utc_now())
+    |> case do
+      :lt -> add_error(changeset, :scheduledFor, "is in the past")
+      _ -> changeset
+    end
   end
 
   def execute(data, state) do
-    # TODO: revisit this contract.
-    case Kousa.Room.create_room(
-           state.user_id,
-           data.name,
-           data.description,
-           data.isPrivate,
-           data.userIdsToInvite
-         ) do
-      {:ok, %{room: room}} ->
-        {:reply, Map.from_struct(room), state}
-
-      error = {:error, _} ->
-        {:reply, %{error: error}}
-    end
   end
 end
--- kousa/test/broth/_messages/room/create_scheduled_test.exs
+++ kousa/test/broth/_messages/room/create_scheduled_test.exs
@@ -3,60 +3,125 @@ defmodule BrothTest.Message.Room.CreateScheduledTest do
 
   @moduletag :message
 
-  alias Beef.Schemas.ScheduledRoom
   alias Broth.Message.Room.CreateScheduled
 
   setup do
-    {:ok, uuid: UUID.uuid4()}
+    future = DateTime.utc_now() |> DateTime.add(1, :second)
+    {:ok, uuid: UUID.uuid4(), future: future}
   end
 
-  describe "when you send an create_scheduled message to change name" do
-    test "it populates create_scheduled fields", %{uuid: uuid} do
-      assert {:ok,
-              %{
-                payload: %CreateScheduled{room: %ScheduledRoom{name: "foobar"}}
-              }} =
+  describe "when you send an create_scheduled message" do
+    test "it populates create_scheduled fields", %{uuid: uuid, future: future} do
+      assert {:ok, %{payload: %CreateScheduled{name: "foobar"}}} =
                BrothTest.Support.Message.validate(%{
                  "operator" => "room:create_scheduled",
-                 "payload" => %{"name" => "foobar", "description" => "a room"},
+                 "payload" => %{"name" => "foobar", "scheduledFor" => future},
                  "reference" => uuid
                })
 
       # short form also allowed
-      assert {:ok,
-              %{
-                payload: %CreateScheduled{room: %ScheduledRoom{name: "foobar"}}
-              }} =
+      assert {:ok, %{payload: %CreateScheduled{name: "foobar"}}} =
                BrothTest.Support.Message.validate(%{
                  "op" => "room:create_scheduled",
-                 "p" => %{"name" => "foobar", "description" => "a room"},
+                 "p" => %{"name" => "foobar", "scheduledFor" => future},
                  "ref" => uuid
                })
     end
 
-    test "omitting the reference is not allowed" do
+    test "omitting the reference is not allowed", %{future: future} do
       assert {:error,
               %{errors: [reference: {"is required for Broth.Message.Room.CreateScheduled", _}]}} =
                BrothTest.Support.Message.validate(%{
                  "operator" => "room:create_scheduled",
-                 "payload" => %{"name" => "foobar", "description" => "a room"}
+                 "payload" => %{"name" => "foobar", "scheduledFor" => future}
                })
     end
 
-    test "providing the wrong datatype for name is disallowed", %{uuid: uuid} do
+    test "providing the wrong datatype for name is disallowed", %{uuid: uuid, future: future} do
       assert {:error, %{errors: [name: {"is invalid", _}]}} =
                BrothTest.Support.Message.validate(%{
                  "operator" => "room:create_scheduled",
-                 "payload" => %{"name" => ["foobar", "barbaz"], "description" => "a room"},
+                 "payload" => %{"name" => ["foobar", "barbaz"], "scheduledFor" => future},
                  "reference" => uuid
                })
     end
 
-    test "providing the wrong datatype for description is disallowed", %{uuid: uuid} do
+    test "omitting the name is disallowed", %{uuid: uuid, future: future} do
+      assert {:error, %{errors: [name: {"can't be blank", _}]}} =
+               BrothTest.Support.Message.validate(%{
+                 "operator" => "room:create_scheduled",
+                 "payload" => %{"scheduledFor" => future},
+                 "reference" => uuid
+               })
+    end
+  end
+
+  describe "when you send an create_scheduled message the scheduledFor field" do
+    test "strings are ok", %{uuid: uuid, future: future} do
+      assert {:ok, %{payload: %CreateScheduled{scheduledFor: ^future}}} =
+               BrothTest.Support.Message.validate(%{
+                 "operator" => "room:create_scheduled",
+                 "payload" => %{"name" => "foobar", "scheduledFor" => "#{future}"},
+                 "reference" => uuid
+               })
+    end
+
+    test "providing the wrong datatype is disallowed", %{uuid: uuid} do
+      assert {:error, %{errors: [scheduledFor: {"is invalid", _}]}} =
+               BrothTest.Support.Message.validate(%{
+                 "operator" => "room:create_scheduled",
+                 "payload" => %{"name" => "foobar", "scheduledFor" => "xxxx"},
+                 "reference" => uuid
+               })
+    end
+
+    test "omitting is disallowed", %{uuid: uuid} do
+      assert {:error, %{errors: [scheduledFor: {"can't be blank", _}]}} =
+               BrothTest.Support.Message.validate(%{
+                 "operator" => "room:create_scheduled",
+                 "payload" => %{"name" => "foobar"},
+                 "reference" => uuid
+               })
+    end
+
+    test "a time in the past is disallowed", %{uuid: uuid} do
+      past = DateTime.utc_now() |> DateTime.add(-1, :second)
+
+      assert {:error, %{errors: [scheduledFor: {"is in the past", _}]}} =
+        BrothTest.Support.Message.validate(%{
+          "operator" => "room:create_scheduled",
+          "payload" => %{"name" => "foobar", "scheduledFor" => past},
+          "reference" => uuid
+        })
+    end
+  end
+
+  describe "when you send an create_scheduled message the description field" do
+    test "providing a description is allowed", %{uuid: uuid, future: future} do
+      assert {:ok, %{payload: %CreateScheduled{description: "barbaz"}}} =
+               BrothTest.Support.Message.validate(%{
+                 "operator" => "room:create_scheduled",
+                 "payload" => %{
+                   "name" => "foobar",
+                   "scheduledFor" => future,
+                   "description" => "barbaz"
+                 },
+                 "reference" => uuid
+               })
+    end
+
+    test "providing the wrong datatype for description is disallowed", %{
+      uuid: uuid,
+      future: future
+    } do
       assert {:error, %{errors: [description: {"is invalid", _}]}} =
                BrothTest.Support.Message.validate(%{
                  "operator" => "room:create_scheduled",
-                 "payload" => %{"name" => "foobar", "description" => ["a", "room"]},
+                 "payload" => %{
+                   "name" => "foobar",
+                   "scheduledFor" => future,
+                   "description" => ["foo"]
+                 },
                  "reference" => uuid
                })
     end
