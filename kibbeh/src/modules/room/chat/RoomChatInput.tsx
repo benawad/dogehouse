018147adb04712c175589fe52d8aef40274db9bf
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-04-15 10:24:45 -0600

    refactor(global): revert changes from pull#2008

--- kibbeh/src/modules/room/chat/RoomChatInput.tsx
+++ kibbeh/src/modules/room/chat/RoomChatInput.tsx
@@ -48,10 +48,6 @@ export const RoomChatInput: React.FC<ChatInputProps> = ({ users }) => {
     if (!open) inputRef.current?.focus();
   }, [open]);
 
-  useEffect(() => () => {
-    setMessage("");
-  }, []);
-
   const handleSubmit = (
     e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
   ) => {
@@ -113,7 +109,7 @@ export const RoomChatInput: React.FC<ChatInputProps> = ({ users }) => {
           }}
         />
       </div>
-      <div className="flex items-stretch">
+      <div className="flex flex items-stretch">
         <div className="flex flex-1 mr-2 lg:mr-0 items-center bg-primary-700 rounded-8">
           <Input
             maxLength={512}
@@ -135,7 +131,7 @@ export const RoomChatInput: React.FC<ChatInputProps> = ({ users }) => {
             }}
           />
           <div
-            className={`right-12 cursor-pointer flex flex-row-reverse fill-current text-primary-200 mr-3`}
+            className={`flex right-12 cursor-pointer flex flex-row-reverse fill-current text-primary-200 mr-3`}
             onClick={() => {
               setOpen(!open);
               position = 0;
