da20916c9330e0bf9ea429b86b220e236243001b
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2021-04-17 11:23:52 +0300

    style(kibbeh): remove log statement

--- kibbeh/src/shared-hooks/useOnClickOutside.tsx
+++ kibbeh/src/shared-hooks/useOnClickOutside.tsx
@@ -3,7 +3,6 @@ import { useEffect, RefObject } from "react";
 export const useOnClickOutside = (ref: RefObject<any>, handler: Function) => {
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
-      console.log(ref.current, event.target);
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
