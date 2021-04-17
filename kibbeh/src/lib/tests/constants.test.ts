ba4eef263b0d49443dbde6c584874f4735a3e1cd
Aksh Bansal <akshmark@gmail.com>
2021-04-17 20:13:01 +0530

    Grouped link regex tests

--- kibbeh/src/lib/tests/constants.test.ts
+++ kibbeh/src/lib/tests/constants.test.ts
@@ -1,25 +1,27 @@
 import { linkRegex } from "../constants";
 
-test("Link Regex Test 1(Only Link)", () => {
-  const msg1 = "https://abc.com";
+describe("Link Regex", () => {
+  test("Link Regex Test 1(Only Link)", () => {
+    const msg1 = "https://abc.com";
 
-  expect(linkRegex.test(msg1)).toBeTruthy();
-});
+    expect(linkRegex.test(msg1)).toBeTruthy();
+  });
 
-test("Link Regex Test 2(Link between text)", () => {
-  const msg2 = "some text https://abc.com other text";
+  test("Link Regex Test 2(Link between text)", () => {
+    const msg2 = "some text https://abc.com other text";
 
-  expect(linkRegex.test(msg2)).toBeTruthy();
-});
+    expect(linkRegex.test(msg2)).toBeTruthy();
+  });
 
-test("Link Regex Test 3(Link in brackets)", () => {
-  const msg3 = "(https://abc.com)";
+  test("Link Regex Test 3(Link in brackets)", () => {
+    const msg3 = "(https://abc.com)";
 
-  expect(linkRegex.test(msg3)).toBeFalsy();
-});
+    expect(linkRegex.test(msg3)).toBeFalsy();
+  });
 
-test("Link Regex Test 4(Link with parameters)", () => {
-  const msg4 = "text after https://abc.com/queries?parameter text after";
+  test("Link Regex Test 4(Link with parameters)", () => {
+    const msg4 = "text after https://abc.com/queries?parameter text after";
 
-  expect(linkRegex.test(msg4)).toBeTruthy();
+    expect(linkRegex.test(msg4)).toBeTruthy();
+  });
 });
