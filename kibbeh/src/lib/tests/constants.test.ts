85bc296c6de482a55a673b2907e14147b26e6ebe
Aksh Bansal <akshmark@gmail.com>
2021-04-17 20:33:27 +0530

    Improved link regex tests

--- kibbeh/src/lib/tests/constants.test.ts
+++ kibbeh/src/lib/tests/constants.test.ts
@@ -1,25 +1,25 @@
 import { linkRegex } from "../constants";
 
 describe("Link Regex", () => {
-  test("Link Regex Test 1(Only Link)", () => {
+  test("Only Link", () => {
     const msg1 = "https://abc.com";
 
     expect(linkRegex.test(msg1)).toBeTruthy();
   });
 
-  test("Link Regex Test 2(Link between text)", () => {
+  test("Link between text", () => {
     const msg2 = "some text https://abc.com other text";
 
     expect(linkRegex.test(msg2)).toBeTruthy();
   });
 
-  test("Link Regex Test 3(Link in brackets)", () => {
+  test("Link in brackets", () => {
     const msg3 = "(https://abc.com)";
 
     expect(linkRegex.test(msg3)).toBeFalsy();
   });
 
-  test("Link Regex Test 4(Link with parameters)", () => {
+  test("Link with parameters", () => {
     const msg4 = "text after https://abc.com/queries?parameter text after";
 
     expect(linkRegex.test(msg4)).toBeTruthy();
