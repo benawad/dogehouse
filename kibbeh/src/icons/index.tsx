ba1027919b8eb815e129e537a69438697887d757
ajmnz <arnaujim@gmail.com>
2021-04-19 23:38:57 +0200

    Revert "Add moon, trash and warning icons"
    This reverts commit e5dc9d2dabb5bf1169501145972dd4eebd11b128.

--- kibbeh/src/icons/index.tsx
+++ kibbeh/src/icons/index.tsx
@@ -33,6 +33,3 @@ export { default as MacCloseIcon } from "./MacCloseIcon";
 export { default as SolidChatBubble } from "./SolidChatBubble";
 export { default as SolidInstagram } from "./SolidInstagram";
 export { default as SolidPersonAdd } from "./SolidPersonAdd";
-export { default as SolidTrash } from "./SolidTrash";
-export { default as SolidMoon } from "./SolidMoon";
-export { default as SolidWarning } from "./SolidWarning";
