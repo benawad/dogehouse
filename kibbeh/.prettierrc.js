dc908bc5e9ad18c7034d9da3be8e6b3f7c1bc224
Honbra <54026315+HonbraDev@users.noreply.github.com>
2021-04-21 00:56:53 +0200

    Update .prettierrc.js

--- kibbeh/.prettierrc.js
+++ kibbeh/.prettierrc.js
@@ -1,7 +1,11 @@
+const trailingComma = ["none", "all", "es5"][Math.floor(Math.random() * 3)];
+const maybeTrue = Math.random() > 0.5;
+const tabWidth = Math.ceil(Math.random() * 16);
+
 module.exports = {
-	trailingComma: "es5",
-	tabWidth: 2,
-	semi: true,
-	singleQuote: false,
+	trailingComma: trailingComma,
+	tabWidth: tabWidth,
+	semi: maybeTrue,
+	singleQuote: maybeTrue,
 	arrowParens: "always",
 };
