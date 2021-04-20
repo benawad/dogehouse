c57fd9bcef8cc66b24246643131d3a9dbcac123a
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-04-19 19:41:56 -0600

    refactor(kibbeh): pull#2084

--- kibbeh/src/ui/ProfileTabs.tsx
+++ kibbeh/src/ui/ProfileTabs.tsx
@@ -8,35 +8,35 @@ export const ProfileTabs: React.FC<ProfileTabsProps> = ({ activeTab }) => {
    return (
       <div className="w-full flex items-center justify-around">
          <button 
-            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-transparent hover:border-accent 
+            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-primary-900 hover:border-accent 
                ${activeTab == "about" && `border-accent text-accent`}`
             }
          >
             About
          </button>
          <button 
-            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-transparent hover:border-accent 
+            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-primary-900 hover:border-accent 
                ${activeTab == "rooms" && `border-accent text-accent`}`
             }
          >
             Rooms
          </button>
          <button 
-            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-transparent hover:border-accent 
+            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-primary-900 hover:border-accent 
                ${activeTab == "scheduled" && `border-accent text-accent`}`
             }
          >
             Scheduled
          </button>
          <button 
-            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-transparent hover:border-accent 
+            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-primary-900 hover:border-accent 
                ${activeTab == "recorded" && `border-accent text-accent`}`
             }
          >
             Recorded
          </button>
          <button 
-            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-transparent hover:border-accent 
+            className={`py-1 text-primary-100 text-base font-bold border-b-2 border-primary-900 hover:border-accent 
                ${activeTab == "clips" && `border-accent text-accent`}`
             }
          >
