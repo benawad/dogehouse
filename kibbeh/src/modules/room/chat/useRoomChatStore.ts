a38c3f94577fe36942d79a08a002422febf106af
Ben Awad <benawad97@gmail.com>
2021-04-21 13:08:31 -0500

    Revert "fix(kibbeh): migrate react-virtual to react-virtuoso"
    This reverts commit ad29d9dd254af8fc451e64c249c5318e8f555a2a.
    
    Conflicts:
    	kibbeh/src/modules/room/chat/RoomChatList.tsx
    	yarn.lock

--- kibbeh/src/modules/room/chat/useRoomChatStore.ts
+++ kibbeh/src/modules/room/chat/useRoomChatStore.ts
@@ -94,8 +94,7 @@ export const useRoomChatStore = create(
             { ...m, color: generateColorFromString(m.userId) },
             ...(s.messages.length <= 100 || s.frozen
               ? s.messages
-              : s.messages.slice(s.message.length - 100, s.messages.length)),
-            { ...m, color: generateColorFromString(m.userId) },
+              : s.messages.slice(0, 100)),
           ],
         })),
       setMessages: (messages: RoomChatMessage[]) =>
