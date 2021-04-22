3ad0dbdaf2e06b29163880b9ba11b258f54bd978
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-04-22 00:59:52 -0700

    bumps the version because there is a behavioural change even if the API hasn't changed

--- kousa/mix.exs
+++ kousa/mix.exs
@@ -4,7 +4,7 @@ defmodule Kousa.MixProject do
   def project do
     [
       app: :kousa,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
