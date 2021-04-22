f1a4bbd80b654aa0c3f3e77f3ee3d07533b84590
Akshay <akshay365@icloud.com>
2021-04-22 00:47:11 -0500

    feat(kibbeh): updated code using prettier
    Updated using prettier

--- kibbeh/src/ui/UserWideButton.tsx
+++ kibbeh/src/ui/UserWideButton.tsx
@@ -1,94 +1,93 @@
 import React, { useEffect, useState } from "react";
-import { SingleUser } from "./UserAvatar";
-import { Button } from "./Button";
-import { useTypeSafeTranslation } from "../shared-hooks/useTypeSafeTranslation";
-import { BaseUser } from "@dogehouse/kebab";
+import { SingleUser } from './UserAvatar';
+import { Button } from './Button';
+import { useTypeSafeTranslation } from '../shared-hooks/useTypeSafeTranslation';
+import { BaseUser } from '@dogehouse/kebab';
 
 export interface UserWideButtonInfoProps {
-  user: BaseUser;
+    user: BaseUser;
+
 }
 
 const divStyle = {
-  width: "719px",
-  height: "143px",
-  left: "55px",
-  top: "91px",
-  background: "#151A21",
+    width: '719px',
+    height: '143px',
+    left: '55px',
+    top: '91px',
+    background: '#151A21'
+
 };
 
 const avatarStyle = {
-  width: "44px",
-  height: "44px",
-  left: "-2px",
-  top: "-3px",
-  marginLeft: "57px",
-  // marginRight:'618px',
-  marginBottom: "53px",
-  marginTop: "46px",
-};
+    width: '44px',
+    height: '44px',
+    left: '-2px',
+    top: '-3px',
+    marginLeft:'57px',
+    // marginRight:'618px',
+    marginBottom:'53px',
+    marginTop: '46px'
+
+}
 
 const buttonStyle = {
-  // position: 'static',
-  width: "90px",
-  height: "22px",
-  marginTop: "60px",
-  marginRight: "59px",
-  marginLeft: "250px",
-  marginBottom: "61px",
+    // position: 'static',
+    width: '90px',
+    height: '22px',
+    marginTop: '60px',
+    marginRight: '59px',
+    marginLeft: '250px',
+    marginBottom: '61px'
 };
 
 const testStyle = {
-  width: "81px",
-  height: "22px",
-  left: "70px",
-  top: "0px",
-  color: "#DEE3EA",
-};
+    width: '81px',
+    height: '22px',
+    left: '70px',
+    top: '0px',
+    color: '#DEE3EA'
+}
+
 
 export const UserWideButton: React.FC<UserWideButtonInfoProps> = ({ user }) => {
-  const { t } = useTypeSafeTranslation();
-
-  const [buttonText, setButtonText] = useState("Unblock");
-
-  const changeText = (text: React.SetStateAction<string>) => {
-    console.log({ user });
-    if (buttonText == "Unblock") {
-      setButtonText(text);
-    } else {
-      setButtonText("Unblock");
-    }
-  };
-
-  return (
-    <>
-      <div style={divStyle} className="flex flex-row items-center ">
-        <div style={avatarStyle} className="flex mr-0.5">
-          <SingleUser size="md" src={user.avatarUrl} username={user.username} />
-        </div>
-        <div className="flex flex-col px-2">
-          <span
-            className="flex text-primary-100 mr-3 font-bold h-full break-all line-clamp-1 truncate"
-            style={testStyle}
-          >
-            {user.displayName}
-          </span>
-          <span
-            style={{ color: "#5D7290" }}
-            className="flex text-primary-300 mr-3"
-          >
-            @{user.username}
-          </span>
-        </div>
-        <div style={buttonStyle}>
-          <Button
-            size="big"
-            color="primary"
-            onClick={() => changeText("Block")}
-          >
+    const { t } = useTypeSafeTranslation();
+    
+    const [buttonText, setButtonText] = useState("Unblock");
+    
+    const changeText = (text: React.SetStateAction<string>) => {
+        console.log({user});
+        if (buttonText == "Unblock"){
+            setButtonText(text);
+        } else{
+            setButtonText("Unblock");
+        }
+    
+    };
+    
+    
+    return (
+      <>
+        <div style={divStyle} className="flex flex-row items-center " >
+        <div style={avatarStyle} className="flex mr-" >
+          <SingleUser
+            size="md"
+            src={user.avatarUrl}
+            username={user.username}
+          />
+          </div>
+          <div className="flex flex-col px-2">
+            <span className="flex text-primary-100 mr-3 font-bold h-full break-all line-clamp-1 truncate" style={testStyle}>
+              {user.displayName}
+            </span>
+            <span style={{color: "#5D7290"}} className="flex text-primary-300 mr-3">@{user.username}</span>
+          </div>
+          <div style={buttonStyle} >
+          <Button size="big" color="primary" onClick={() => changeText("Block")}>
             {buttonText}
-          </Button>
+        </Button>
         </div>
-      </div>
-    </>
-  );
-};
+        </div>
+        </>
+    );
+  };
+  
