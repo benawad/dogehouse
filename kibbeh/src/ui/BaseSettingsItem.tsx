520587792ff451c275f0879d35acf62776e6746d
wes337 <wesmoses@gmail.com>
2021-04-22 19:59:03 +0300

    create base settings item component

---
+++ kibbeh/src/ui/BaseSettingsItem.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from "react";
+
+export interface BaseSettingsItemProps {
+  children: ReactNode;
+}
+
+export const BaseSettingsItem: React.FC<BaseSettingsItemProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <div className="bg-primary-900 rounded-8" {...props}>
+      {children}
+    </div>
+  );
+};
