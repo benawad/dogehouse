45ea2a47c0c0ee9bd58dcbcc3eb122080c41c01a
Ben Awad <benawad97@gmail.com>
2021-04-23 12:03:27 -0500

    check to make sure room genserver exists

--- kousa/lib/broth/socket_handler.ex
+++ kousa/lib/broth/socket_handler.ex
@@ -218,7 +218,8 @@ defmodule Broth.SocketHandler do
   end
 
   defp dispatch_mediasoup_message(msg, %{user_id: user_id}) do
-    with {:ok, room_id} <- Beef.Users.tuple_get_current_room_id(user_id) do
+    with {:ok, room_id} <- Beef.Users.tuple_get_current_room_id(user_id),
+         [{_, _}] <- Onion.RoomSession.lookup(room_id) do
       voice_server_id = Onion.RoomSession.get(room_id, :voice_server_id)
 
       mediasoup_message =
