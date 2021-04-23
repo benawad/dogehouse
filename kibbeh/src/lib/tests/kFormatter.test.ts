369ceb1fb80ebc7f3339461a1f3150e86c99a706
wes337 <wesmoses@gmail.com>
2021-04-23 21:12:19 +0300

    add k test cases

--- kibbeh/src/lib/tests/kFormatter.test.ts
+++ kibbeh/src/lib/tests/kFormatter.test.ts
@@ -4,4 +4,24 @@ describe("Number formatter", () => {
   it("should return 1", () => {
     expect(kFormatter(1)).toBe("1");
   });
+
+  it("should return 1k", () => {
+    expect(kFormatter(1000)).toBe("1k");
+  });
+
+  it("should return 3.8k", () => {
+    expect(kFormatter(3821)).toBe("3.8k");
+  });
+
+  it("should return 9.9k", () => {
+    expect(kFormatter(9999)).toBe("9.9k");
+  });
+
+  it("should return 10k", () => {
+    expect(kFormatter(10500)).toBe("10k");
+  });
+
+  it("should return 101k", () => {
+    expect(kFormatter(101800)).toBe("101k");
+  });
 });
