712a89579dd9717954de2cae412e7ae8ced37c83
Ben Awad <benawad97@gmail.com>
2021-04-23 09:44:23 -0500

    lint

--- kibbeh/src/modules/sound-effects/useSoundEffectStore.ts
+++ kibbeh/src/modules/sound-effects/useSoundEffectStore.ts
@@ -7,7 +7,7 @@ export const soundEffects = {
   mute: "mute.wav",
   roomInvite: "roomInvite.wav",
   deafen: "mute.wav",
-  undeafen: "unmute.wav"
+  undeafen: "unmute.wav",
 };
 
 export type PossibleSoundEffect = keyof typeof soundEffects;
@@ -21,7 +21,7 @@ function getInitialSettings() {
     mute: true,
     roomInvite: true,
     deafen: true,
-    undeafen: true
+    undeafen: true,
   };
 
   try {
@@ -29,7 +29,7 @@ function getInitialSettings() {
       const v = localStorage.getItem(keyToLocalStorageKey(key)) || "";
       soundEffectSettings[key as PossibleSoundEffect] = !v || v === "true";
     });
-  } catch { }
+  } catch {}
 
   return soundEffectSettings;
 }
@@ -44,7 +44,7 @@ export const useSoundEffectStore = create(
       setSetting: (key: PossibleSoundEffect, value: boolean) => {
         try {
           localStorage.setItem(keyToLocalStorageKey(key), value.toString());
-        } catch { }
+        } catch {}
         set((x) => ({
           settings: { ...x.settings, [key]: value },
         }));
