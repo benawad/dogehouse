4edbcb3801340e65db3eee1a88df8849aa4b4d91
wes337 <wesmoses@gmail.com>
2021-04-23 21:11:36 +0300

    rewrite kformatter fn

--- kibbeh/src/lib/kFormatter.ts
+++ kibbeh/src/lib/kFormatter.ts
@@ -1,15 +1,10 @@
-export const kFormatter = (num: number) => {
-  if (Math.abs(num) > 999999999) {
-    return `${
-      Math.sign(num) * Number((Math.abs(num) / 1000000000).toFixed(1))
-    }b`;
-  }
-  if (Math.abs(num) > 999999) {
-    return `${Math.sign(num) * Number((Math.abs(num) / 1000000).toFixed(1))}m`;
-  }
-  if (Math.abs(num) > 999) {
-    return `${Math.sign(num) * Number((Math.abs(num) / 1000).toFixed(1))}k`;
+export const kFormatter = (n: number) => {
+  if (n < 1000) {
+    return `${n}`;
   }
 
-  return Math.sign(num) * Math.abs(num);
+  const base = Math.floor(Math.log(Math.abs(n)) / Math.log(1000));
+  const suffix = "kmb"[base - 1];
+  const abbrev = String(n / 1000 ** base).substring(0, 3);
+  return (abbrev.endsWith(".") ? abbrev.slice(0, -1) : abbrev) + suffix;
 };
