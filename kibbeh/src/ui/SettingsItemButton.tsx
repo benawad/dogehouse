1ddc7ffb05e74e47656587744a8c56b06a6b76ed
Hugo Rydman <carlhugo.rydman@gmail.com>
2021-04-23 17:32:47 +0200

    Made the component more flexible

--- kibbeh/src/ui/SettingsItemButton.tsx
+++ kibbeh/src/ui/SettingsItemButton.tsx
@@ -6,6 +6,7 @@ export type SettingsItemButtonProps = {
   text: string,
   buttonText: string,
   disabled?: boolean,
+  className: string,
   onClick: (e: React.MouseEvent | React.TouchEvent) => void,
 };
 
@@ -13,12 +14,21 @@ export const SettingsItemButton: React.FC<SettingsItemButtonProps> = ({
   text,
   buttonText,
   disabled = false,
+  className = "",
   onClick,
 }) => {
   return (
-    <BaseSettingsItem className="p-4">
-      <p className="text-primary-200 mb-4">{text}</p>
-      <Button color="secondary-accent" size="small" onClick={onClick} disabled={disabled} className="py-2" >
+    <BaseSettingsItem className={`p-4 ${className}`}>
+      <p className="text-primary-200 mb-4">
+        {text}
+      </p>
+      <Button
+        color="secondary-accent"
+        size="small"
+        onClick={onClick}
+        disabled={disabled}
+        className="py-2"
+      >
         <p className="font-bold">
           {buttonText}
         </p>
