5ca45ac5cb9af818e460248d68161e03c892d8a0
ajmnz <arnaujim@gmail.com>
2021-04-24 14:31:30 +0200

    Match Figma component

--- kibbeh/src/stories/ErrorButtonItem.story.tsx
+++ kibbeh/src/stories/ErrorButtonItem.story.tsx
@@ -3,22 +3,22 @@ import { Story } from "@storybook/react";
 import { ErrorButtonItem, ErrorButtonItemProps } from "../ui/ErrorButtonItem";
 
 export default {
-   title: "ErrorButtonItem",
-   component: ErrorButtonItem
-}
+  title: "ErrorButtonItem",
+  component: ErrorButtonItem,
+};
 
 export const Main: Story<ErrorButtonItemProps> = ({
-   actionButtonText = "Refresh",
-   errorMessageHeading = "No microphone found",
-   errorMessageText = "You either have no device plugged in or haven’t given this website permission"
+  actionButtonText = "Refresh",
+  errorMessageHeading = "No microphone found",
+  errorMessageText = "You either have no device plugged in or haven’t given this website permission",
 }) => {
-   return (
-      <div className="w-2/5 mx-auto">
-         <ErrorButtonItem
-            actionButtonText={actionButtonText}
-            errorMessageHeading={errorMessageHeading}
-            errorMessageText={errorMessageText}
-         />
-      </div>
-   );
-}
+  return (
+    <div className="mx-auto" style={{ width: 600 }}>
+      <ErrorButtonItem
+        actionButtonText={actionButtonText}
+        errorMessageHeading={errorMessageHeading}
+        errorMessageText={errorMessageText}
+      />
+    </div>
+  );
+};
