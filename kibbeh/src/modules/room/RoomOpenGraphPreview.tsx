e2180fcec6eefaf783d986d903f813a776699d52
Ben Awad <benawad97@gmail.com>
2021-04-23 19:05:39 -0500

    ssr room page

--- kibbeh/src/modules/room/RoomOpenGraphPreview.tsx
+++ kibbeh/src/modules/room/RoomOpenGraphPreview.tsx
@@ -1,26 +1,21 @@
 import { Room } from "@dogehouse/kebab";
-import { useRouter } from "next/router";
 import React from "react";
-import { useQuery } from "react-query";
-import { validate } from "uuid";
 import { isServer } from "../../lib/isServer";
 import { HeaderController } from "../display/HeaderController";
 
-interface RoomOpenGraphPreviewProps {}
+interface RoomOpenGraphPreviewProps {
+  room: Room | null | undefined;
+}
 
 export const RoomOpenGraphPreview: React.FC<RoomOpenGraphPreviewProps> = ({
+  room,
   children,
 }) => {
-  const { query } = useRouter();
-  const id = typeof query.id === "string" ? query.id : "";
-  const key = `/room/${id}`;
-  const { data } = useQuery<{ room: Room } | { error: string }>(key, {
-    enabled: isServer && validate(id),
-  });
-
-  if (isServer && data && "room" in data) {
-    const { name, description } = data.room;
-    return <HeaderController title={name} description={description} />;
+  if (isServer && room) {
+    const { name, description } = room;
+    return (
+      <HeaderController title={name} description={description} embed={{}} />
+    );
   }
 
   return <>{children}</>;
--- kibbeh/src/modules/room/RoomPage.tsx
+++ kibbeh/src/modules/room/RoomPage.tsx
@@ -1,24 +1,29 @@
+import { Room } from "@dogehouse/kebab";
 import { useRouter } from "next/router";
 import React from "react";
+import { validate } from "uuid";
+import { isServer } from "../../lib/isServer";
+import { defaultQueryFn } from "../../lib/defaultQueryFn";
 import { PageComponent } from "../../types/PageComponent";
 import { WaitForWsAndAuth } from "../auth/WaitForWsAndAuth";
 import { FollowingOnlineController } from "../dashboard/FollowingOnlineController";
 import { DesktopLayout } from "../layouts/DesktopLayout";
-import { LeftPanel } from "../layouts/GridPanels";
 import { TabletSidebar } from "../layouts/TabletSidebar";
 import { RoomChatController } from "./RoomChatController";
 import { RoomOpenGraphPreview } from "./RoomOpenGraphPreview";
 import { RoomPanelController } from "./RoomPanelController";
 import { UserPreviewModalProvider } from "./UserPreviewModalProvider";
 
-interface RoomPageProps {}
+interface RoomPageProps {
+  room?: Room;
+}
 
-export const RoomPage: PageComponent<RoomPageProps> = ({}) => {
+export const RoomPage: PageComponent<RoomPageProps> = ({ room }) => {
   const { query } = useRouter();
   const key = typeof query.id === "string" ? query.id : "";
 
   return (
-    <RoomOpenGraphPreview>
+    <RoomOpenGraphPreview room={room}>
       <WaitForWsAndAuth>
         <UserPreviewModalProvider>
           <DesktopLayout
@@ -36,3 +41,20 @@ export const RoomPage: PageComponent<RoomPageProps> = ({}) => {
 };
 
 RoomPage.ws = true;
+// ssr
+RoomPage.getInitialProps = async ({ query }) => {
+  const key =
+    typeof query.id === "string" && validate(query.id) ? query.id : "";
+  let room = null;
+
+  if (isServer && key) {
+    try {
+      const resp = await defaultQueryFn({ queryKey: `/room/${key}` });
+      if ("room" in resp) {
+        room = resp.room;
+      }
+    } catch {}
+  }
+
+  return { room };
+};
