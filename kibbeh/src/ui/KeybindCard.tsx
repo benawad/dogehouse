0b18da6da555cac0afc017f66436f23f4e778038
ajmnz <arnaujim@gmail.com>
2021-04-24 14:53:46 +0200

    Use Tag component, eslint and prettier

--- kibbeh/src/ui/KeybindCard.tsx
+++ kibbeh/src/ui/KeybindCard.tsx
@@ -2,87 +2,86 @@ import React from "react";
 import { BaseSettingsItem } from "./BaseSettingsItem";
 import { Button } from "./Button";
 import SolidKeyboardIcon from "../icons/SolidKeyboard";
+import { Tag } from "./Tag";
 
-const modifiers =  {
-   ctrl: "ctrlkey",
-   alt: "altkey",
-   shift: "shiftkey"
-}
+const modifiers = {
+  ctrl: "ctrlkey",
+  alt: "altkey",
+  shift: "shiftkey",
+};
 export interface KeybindCardProps {
-   command: string
-   modifier?: keyof typeof modifiers
-   primaryKey: string
+  command: string;
+  modifier?: keyof typeof modifiers;
+  primaryKey: string;
 }
 
 export const KeybindCard: React.FC<KeybindCardProps> = ({
-   command,
-   modifier,
-   primaryKey
+  command,
+  modifier,
+  primaryKey,
 }) => {
-   const [isListening, setIsListening] = React.useState(false);
+  const [isListening, setIsListening] = React.useState(false);
 
-   React.useEffect(() => {
-      const handleKeybinding = (e: KeyboardEvent) => {
-         e.preventDefault();
-         // wait for key combination to avoid two updates
-         if(["Control", "Alt", "Shift"].includes(e.key)) return;
+  React.useEffect(() => {
+    const handleKeybinding = (e: KeyboardEvent) => {
+      e.preventDefault();
+      // wait for key combination to avoid two updates
+      if (["Control", "Alt", "Shift"].includes(e.key)) return;
 
-         let newModifier;
+      let newModifier = "";
 
-         if(e.ctrlKey) newModifier = "ctrl"
-         if(e.altKey) newModifier = "alt"
-         if(e.shiftKey) newModifier = "shift"
+      if (e.ctrlKey) newModifier = "ctrl";
+      if (e.altKey) newModifier = "alt";
+      if (e.shiftKey) newModifier = "shift";
 
-         const keybind = modifier ?
-            [newModifier, e.key]
-         :
-            e.key
+      const keybind = modifier ? [newModifier, e.key] : e.key;
 
-         // handle update keybind
-         // ...
+      // handle update keybind
+      // ...
 
-         console.log("keybind: ", keybind);
-         document.removeEventListener("keydown", handleKeybinding);
-         setIsListening(false);
-      }
+      console.log("keybind: ", keybind);
+      document.removeEventListener("keydown", handleKeybinding);
+      setIsListening(false);
+    };
 
-      document.addEventListener("keydown", handleKeybinding);
+    document.addEventListener("keydown", handleKeybinding);
 
-      return () => {
-         document.removeEventListener("keydown", handleKeybinding);
-      }
-   }, [isListening]);
+    return () => {
+      document.removeEventListener("keydown", handleKeybinding);
+    };
+  }, [isListening, modifier]);
 
-
-   return (
-      <BaseSettingsItem 
-         className={`
+  return (
+    <BaseSettingsItem
+      className={`
             flex justify-between items-center bg-primary-900 px-3 py-4 border border-transparent
             transition
             ${isListening && "border-accent"}
          `}
-      >
-         <div>
-            <p className="text-base font-bold text-primary-100 capitalize">{command}</p>
-         </div>
-         <div className="flex items-center">
-            {isListening ?
-               <span className="text-xs font-bold px-1 bg-primary-600 text-primary-100 text-center rounded-5 leading-loose uppercase">listening...</span>
-               :
-               <>
-                  <span className="text-xs font-bold px-1 bg-primary-600 text-primary-100 text-center rounded-5 leading-loose uppercase" style={{minWidth: "20px"}}>{modifier}</span>
-                  <span className="text-xs font-bold px-1 bg-primary-600 text-primary-100 text-center rounded-5 leading-loose uppercase ml-2" style={{minWidth: "20px"}}>{primaryKey}</span>
-               </>
-            }
-            <Button
-               size="tiny"
-               color="transparent"
-               className="ml-4"
-               onClick={() => setIsListening(!isListening)}
-            >
-               <SolidKeyboardIcon className="text-primary-100"/>
-            </Button>
-         </div>
-      </BaseSettingsItem>
-   );
-}
+    >
+      <div>
+        <p className="text-base font-bold text-primary-100 capitalize">
+          {command}
+        </p>
+      </div>
+      <div className="flex items-center">
+        {isListening ? (
+          <Tag className="uppercase">Listening...</Tag>
+        ) : (
+          <div className="space-x-1 flex">
+            <Tag className="uppercase">{modifier}</Tag>
+            <Tag className="uppercase">{primaryKey}</Tag>
+          </div>
+        )}
+        <Button
+          size="tiny"
+          color="transparent"
+          className="ml-4"
+          onClick={() => setIsListening(!isListening)}
+        >
+          <SolidKeyboardIcon className="text-primary-100" />
+        </Button>
+      </div>
+    </BaseSettingsItem>
+  );
+};
