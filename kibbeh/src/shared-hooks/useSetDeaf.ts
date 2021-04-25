123627107a672d47f39bfbceadf0a87c56153518
ajmnz <arnaujim@gmail.com>
2021-04-25 13:28:15 +0200

    Set mute on deaf

--- kibbeh/src/shared-hooks/useSetDeaf.ts
+++ kibbeh/src/shared-hooks/useSetDeaf.ts
@@ -1,13 +1,20 @@
 import { Wrapper } from "@dogehouse/kebab";
 import { useDeafStore } from "../global-stores/useDeafStore";
+import { useMuteStore } from "../global-stores/useMuteStore";
 import { useWrappedConn } from "./useConn";
 
 export const useSetDeaf = () => {
   const conn = useWrappedConn();
   const { setInternalDeaf } = useDeafStore();
+  const { setInternalMute } = useMuteStore();
   return (deaf: boolean) => {
     setInternalDeaf(deaf);
     conn.mutation.setDeaf(deaf);
+
+    if (deaf) {
+      setInternalMute(true);
+      conn.mutation.setMute(true);
+    }
   };
 };
 
