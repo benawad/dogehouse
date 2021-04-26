4c24568fe05ac0cdac9621dc993d5922ca0cd169
Ben Awad <benawad97@gmail.com>
2021-04-26 12:15:25 -0500

    fix(kousa): #2342, #2310

--- kousa/lib/broth/routes/twitter_auth.ex
+++ kousa/lib/broth/routes/twitter_auth.ex
@@ -5,6 +5,7 @@ defmodule Broth.Routes.TwitterAuth do
   require Logger
   alias Beef.Users
   alias Kousa.Utils.Urls
+  alias Broth.Plugs.Redirect
 
   plug(:put_secret_key_base)
 
@@ -22,11 +23,15 @@ defmodule Broth.Routes.TwitterAuth do
   end
 
   get "/web" do
-    %{conn | params: Map.put(conn.params, "state", "web")}
-    |> Plug.Conn.put_private(:ueberauth_request_options, %{
-      callback_url: Application.get_env(:kousa, :api_url) <> "/auth/twitter/callback",
-      options: []
-    })
+    token =
+      ExTwitter.request_token(
+        Application.get_env(:kousa, :api_url) <>
+          "/auth/twitter/callback"
+      )
+
+    {:ok, authenticate_url} = ExTwitter.authenticate_url(token.oauth_token)
+
+    conn
     |> fetch_session()
     |> put_session(
       :redirect_to_next,
@@ -34,22 +39,78 @@ defmodule Broth.Routes.TwitterAuth do
         k == "referer" and Urls.next_site_url?(v)
       end)
     )
-    |> Ueberauth.Strategy.Twitter.handle_request!()
+    |> Redirect.redirect(authenticate_url)
   end
 
   get "/callback" do
-    conn_with_stuff =
+    conn_with_qp =
       conn
       |> fetch_query_params
       |> fetch_session()
 
+    base_url = get_base_url(conn_with_qp)
+
     try do
-      conn_with_stuff
-      |> Plug.Conn.put_private(:ueberauth_request_options, %{
-        options: []
-      })
-      |> Ueberauth.Strategy.Twitter.handle_callback!()
-      |> handle_callback()
+      with %{"oauth_token" => oauth_token, "oauth_verifier" => oauth_verifier} <-
+             conn_with_qp.query_params,
+           {:ok, access_token} <- ExTwitter.access_token(oauth_verifier, oauth_token),
+           _ <-
+             ExTwitter.configure(
+               consumer_key: System.get_env("TWITTER_API_KEY"),
+               consumer_secret: System.get_env("TWITTER_SECRET_KEY"),
+               access_token: access_token.oauth_token,
+               access_token_secret: access_token.oauth_token_secret
+             ),
+           %ExTwitter.Model.User{
+             description: bio,
+             name: displayName,
+             id_str: twitterId,
+             raw_data: %{email: email},
+             profile_image_url_https: avatarUrl,
+             profile_banner_url: bannerUrl
+           } <- ExTwitter.verify_credentials(include_email: true),
+           {_, db_user} <-
+             Users.twitter_find_or_create(%{
+               bio: bio,
+               displayName: displayName,
+               twitterId: twitterId,
+               bannerUrl: bannerUrl,
+               email: email,
+               avatarUrl: avatarUrl
+             }) do
+        if not is_nil(db_user.reasonForBan) do
+          conn
+          |> Redirect.redirect(
+            base_url <>
+              "/?error=" <>
+              URI.encode(
+                "your account got banned, if you think this was a mistake, please send me an email at benawadapps@gmail.com"
+              )
+          )
+        else
+          conn
+          |> Redirect.redirect(
+            base_url <>
+              "/?accessToken=" <>
+              Kousa.AccessToken.generate_and_sign!(%{"userId" => db_user.id}) <>
+              "&refreshToken=" <>
+              Kousa.RefreshToken.generate_and_sign!(%{
+                "userId" => db_user.id,
+                "tokenVersion" => db_user.tokenVersion
+              })
+          )
+        end
+      else
+        x ->
+          IO.inspect(x)
+
+          conn
+          |> Redirect.redirect(
+            base_url <>
+              "/?error=" <>
+              URI.encode("twitter login callback failed for some reason, tell ben to check logs")
+          )
+      end
     rescue
       e ->
         Sentry.capture_exception(e,
@@ -57,9 +118,9 @@ defmodule Broth.Routes.TwitterAuth do
           extra: %{twitter_auth: "/callback"}
         )
 
-        conn_with_stuff
+        conn_with_qp
         |> Broth.Plugs.Redirect.redirect(
-          get_base_url(conn_with_stuff) <>
+          base_url <>
             "/?error=" <>
             URI.encode("auth failed, enable cookies and try again or give GitHub a try")
         )
@@ -74,108 +135,5 @@ defmodule Broth.Routes.TwitterAuth do
       _ ->
         Application.fetch_env!(:kousa, :web_url)
     end
-  end
-
-  def handle_callback(
-        %Plug.Conn{assigns: %{ueberauth_failure: %{errors: [%{message_key: "missing_code"}]}}} =
-          conn
-      ) do
-    conn
-    |> Broth.Plugs.Redirect.redirect(
-      get_base_url(conn) <>
-        "/?error=" <>
-        URI.encode("try again")
-    )
-  end
-
-  def handle_callback(%Plug.Conn{assigns: %{ueberauth_failure: failure}} = conn) do
-    Logger.warn("twitter oauth failure #{inspect(failure)}")
-
-    conn
-    |> Broth.Plugs.Redirect.redirect(
-      get_base_url(conn) <>
-        "/?error=" <>
-        URI.encode(
-          "something went wrong, try again and if the error persists, tell ben to check the server logs"
-        )
-    )
-  end
-
-  def handle_callback(
-        %Plug.Conn{
-          private: %{
-            twitter_user: %{
-              "description" => bio,
-              "name" => displayName,
-              "id_str" => twitterId,
-              "email" => email,
-              "profile_image_url_https" => avatarUrl,
-              "profile_banner_url" => bannerUrl
-            }
-          }
-        } = conn
-      ) do
-    try do
-      {_, db_user} =
-        Users.twitter_find_or_create(%{
-          bio: bio,
-          displayName: displayName,
-          twitterId: twitterId,
-          email: email,
-          avatarUrl: avatarUrl,
-          bannerUrl: bannerUrl
-        })
-
-      if not is_nil(db_user.reasonForBan) do
-        conn
-        |> Broth.Plugs.Redirect.redirect(
-          get_base_url(conn) <>
-            "/?error=" <>
-            URI.encode(
-              "your account got banned, if you think this was a mistake, please send me an email at benawadapps@gmail.com"
-            )
-        )
-      else
-        conn
-        |> Broth.Plugs.Redirect.redirect(
-          get_base_url(conn) <>
-            "/?accessToken=" <>
-            Kousa.AccessToken.generate_and_sign!(%{"userId" => db_user.id}) <>
-            "&refreshToken=" <>
-            Kousa.RefreshToken.generate_and_sign!(%{
-              "userId" => db_user.id,
-              "tokenVersion" => db_user.tokenVersion
-            })
-        )
-      end
-    rescue
-      e ->
-        Logger.warn("twitter login error #{inspect(e)}")
-
-        Sentry.capture_exception(e,
-          stacktrace: __STACKTRACE__,
-          extra: %{rest: "twitter_login"}
-        )
-
-        conn
-        |> Broth.Plugs.Redirect.redirect(
-          get_base_url(conn) <>
-            "/?error=" <>
-            URI.encode("twitter login callback failed for some reason, tell ben to check logs")
-        )
-    end
-  end
-
-  def handle_callback(conn) do
-    Logger.warn("twitter authorization error #{inspect(conn)}")
-
-    conn
-    |> Broth.Plugs.Redirect.redirect(
-      get_base_url(conn) <>
-        "/?error=" <>
-        URI.encode(
-          "something went wrong, try again and if the error persists, tell ben to check the server logs"
-        )
-    )
   end
 end
