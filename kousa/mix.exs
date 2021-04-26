28bcd2c99c103c63a76bcdade9c11d4c7fb694ea
Ben Awad <benawad97@gmail.com>
2021-04-26 07:47:14 -0500

    http request tests

--- kousa/mix.exs
+++ kousa/mix.exs
@@ -42,6 +42,7 @@ defmodule Kousa.MixProject do
       {:elixir_uuid, "~> 1.2"},
       # TODO: switch off of httpoison to, e.g. Mojito or Finch
       {:httpoison, "~> 1.8"},
+      {:finch, "~> 0.6"},
       {:sentry, "~> 8.0"},
       {:postgrex, ">= 0.0.0"},
       {:remix, "~> 0.0.1", only: :dev},
