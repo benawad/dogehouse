ea723e6c023fc585b1d1f55722b5b7086fc8b71d
ajmnz <arnaujim@gmail.com>
2021-04-26 14:00:05 +0200

    Override material theme to match our color palette

--- kibbeh/src/pages/_app.tsx
+++ kibbeh/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "../styles/globals.css";
 import "../styles/electron-header.css";
 import "../styles/banner-button.css";
+import "../styles/date-time-picker.css";
 import { AppProps } from "next/app";
 import { QueryClientProvider } from "react-query";
 import { WebSocketProvider } from "../modules/ws/WebSocketProvider";
