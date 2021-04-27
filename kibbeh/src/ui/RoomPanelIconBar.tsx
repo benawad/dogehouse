c1fbd96ecf96b906c00b3696ba92e51f92460167
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-27 11:41:38 +0300

    fix(kibbeh): mute icon does not change to inactive color when deafened
    mute icon does not change to inactive color when deafened even though it mutes

--- kibbeh/src/ui/RoomPanelIconBar.tsx
+++ kibbeh/src/ui/RoomPanelIconBar.tsx
@@ -46,7 +46,9 @@ export const RoomPanelIconBar: React.FC<RoomPanelIconBarProps> = ({
             transition
             hover={!mute.isMuted}
             className={`mr-2 w-11 h-6.5 ${
-              !mute.isMuted ? `bg-accent hover:bg-accent-hover text-button` : ``
+              !mute.isMuted && !deaf?.isDeaf
+                ? `bg-accent hover:bg-accent-hover text-button`
+                : ``
             }`}
             color="800"
             title={t("components.bottomVoiceControl.toggleMuteMicBtn")}
