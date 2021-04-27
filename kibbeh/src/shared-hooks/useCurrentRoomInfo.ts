2cd59eb689030ab765ad5d793005eb714dcfbb62
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-28 01:31:03 +0300

    fix(kibbeh): isMod notification

--- kibbeh/src/shared-hooks/useCurrentRoomInfo.ts
+++ kibbeh/src/shared-hooks/useCurrentRoomInfo.ts
@@ -1,8 +1,12 @@
+import isElectron from "is-electron";
 import { useContext } from "react";
 import { useCurrentRoomIdStore } from "../global-stores/useCurrentRoomIdStore";
 import { isServer } from "../lib/isServer";
 import { WebSocketContext } from "../modules/ws/WebSocketProvider";
 import { useTypeSafeQuery } from "./useTypeSafeQuery";
+
+let iWasMod = false;
+let ipcRenderer: any = undefined;
 
 export const useCurrentRoomInfo = () => {
   const { currentRoomId } = useCurrentRoomIdStore();
@@ -43,7 +47,13 @@ export const useCurrentRoomInfo = () => {
   }
 
   const isCreator = me.id === data.room.creatorId;
-
+  if (isElectron()) {
+    if (iWasMod != isMod) {
+      iWasMod = isMod
+      ipcRenderer = window.require("electron").ipcRenderer;
+      ipcRenderer.send("@notification/mod", isMod);
+    }
+  }
   return {
     isCreator,
     isMod,
