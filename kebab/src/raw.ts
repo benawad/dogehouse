d58ef39f5f4a27a3b9bd27e22d4c608cd1f10913
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-27 08:58:53 +0300

    kebab: add missing import

--- kebab/src/raw.ts
+++ kebab/src/raw.ts
@@ -58,9 +58,9 @@ export const connect = (
   token: Token,
   refreshToken: Token,
   {
-    logger = () => {},
-    onConnectionTaken = () => {},
-    onClearTokens = () => {},
+    logger = () => { },
+    onConnectionTaken = () => { },
+    onClearTokens = () => { },
     url = apiUrl,
     fetchTimeout,
     getAuthOptions,
@@ -93,9 +93,8 @@ export const connect = (
       if (socket.readyState !== socket.OPEN) {
         return;
       }
-      const raw = `{"v":"0.2.0", "op":"${opcode}","p":${JSON.stringify(data)}${
-        ref ? `,"ref":"${ref}"` : ""
-      }}`;
+      const raw = `{"v":"0.2.0", "op":"${opcode}","p":${JSON.stringify(data)}${ref ? `,"ref":"${ref}"` : ""
+        }}`;
 
       socket.send(raw);
       logger("out", opcode, data, ref, raw);
@@ -108,9 +107,8 @@ export const connect = (
       if (socket.readyState !== socket.OPEN) {
         return;
       }
-      const raw = `{"op":"${opcode}","d":${JSON.stringify(data)}${
-        fetchId ? `,"fetchId":"${fetchId}"` : ""
-      }}`;
+      const raw = `{"op":"${opcode}","d":${JSON.stringify(data)}${fetchId ? `,"fetchId":"${fetchId}"` : ""
+        }}`;
 
       socket.send(raw);
       logger("out", opcode, data, fetchId, raw);
