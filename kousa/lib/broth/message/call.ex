270390c29e1e0a8a41359b3c85b4c048918f0e45
Isaac <isaac.y [at] hey.com>
2021-04-28 23:33:00 -0700

    refactor[kousa]: verified correct compile-time checks

--- kousa/lib/broth/message/call.ex
+++ kousa/lib/broth/message/call.ex
@@ -18,6 +18,8 @@ defmodule Broth.Message.Call do
   `:needs_auth` keyword parameter to false.
   """
 
+  alias Broth.Message.Cast
+
   defmacro __using__(opts) do
     default_reply_module = Module.concat(__CALLER__.module, Reply)
 
@@ -30,26 +32,17 @@ defmodule Broth.Message.Call do
       if reply_module == __CALLER__.module do
         [:inbound, :outbound]
       else
-        [:outbound]
+        [:inbound]
       end
 
     auth_check =
       opts
       |> Keyword.get(:needs_auth, true)
-      |> auth_check()
-
-    schema_ast = if schema = opts[:schema] do
-      quote do
-        Module.register_attribute(__MODULE__, :schema, persist: true)
-        @schema unquote(schema)
-      end
-    end
+      |> Cast.auth_check()
 
     quote do
       use Ecto.Schema
       import Ecto.Changeset
-
-      @after_compile Broth.Message.Call
 
       @behaviour Broth.Message.Call
 
@@ -57,7 +50,7 @@ defmodule Broth.Message.Call do
       @directions unquote(directions)
 
       unquote(auth_check)
-      unquote(schema_ast)
+      unquote(Cast.schema_ast(opts))
 
       @impl true
       def reply_module, do: unquote(reply_module)
@@ -66,21 +59,9 @@ defmodule Broth.Message.Call do
       def initialize(_state), do: struct(__MODULE__)
 
       defoverridable initialize: 1
-    end
-  end
 
-  def auth_check(true) do
-    quote do
-      @impl true
-      def auth_check(%{user: nil}), do: {:error, :auth}
-      def auth_check(_), do: :ok
-    end
-  end
-
-  def auth_check(false) do
-    quote do
-      @impl true
-      def auth_check(_), do: :ok
+      # verify compile-time guarantees
+      @after_compile Broth.Message.Call
     end
   end
 
@@ -104,35 +85,12 @@ defmodule Broth.Message.Call do
   def __after_compile__(%{module: module}, _bin) do
     # checks to make sure you've either declared a schema module, or you have
     # implemented a schema
-
-    case {module.__info__(:attributes)[:schema], function_exported?(module, :__schema__, 2)} do
-      {nil, true} -> :ok
-      {[schema_mod], true} -> raise CompileError, description: "in module #{inspect module} you have both declared the schema module #{inspect schema_mod} and implemented a schema"
-      {nil, false} -> raise CompileError, description: "in module #{inspect module} you have failed to declare a schema module or implement a schema"
-      {[schema_mod], false} ->
-        Code.ensure_loaded?(schema_mod)
-        unless function_exported?(schema_mod, :__schema__, 2) do
-          raise CompileError, description: "in module #{inspect module} you declared the schema #{inspect schema_mod} but it doesn't appear to have a schema"
-        end
-    end
+    Cast.check_for_schema(module, :inbound)
 
     # checks to make sure the declared reply module actually exists.
     reply_module = module.reply_module()
-    Code.ensure_loaded?(reply_module)
+    Code.ensure_compiled(reply_module)
 
-    directions = :attributes
-    |> reply_module.__info__
-    |> Keyword.get(:directions)
-    |> List.wrap
-
-    unless :outbound in directions do
-      raise CompileError,
-        description: "reply module #{inspect(reply_module)} does not seem to be a outbound module"
-    end
-
-    unless function_exported?(reply_module, :__schema__, 2) do
-      raise CompileError,
-        description: "in module #{inspect module} you declared the reply module #{inspect reply_module} but it doesn't appear to have a schema"
-    end
+    Cast.check_for_schema(reply_module, :outbound)
   end
 end
