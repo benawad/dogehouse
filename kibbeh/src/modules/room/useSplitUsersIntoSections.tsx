1da4d52d5c2c68b3c0b5a9f13ae9ebe0373e98c7
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-29 20:10:59 +0300

    fix(kibbeh): remove unnecessary boolean thingy

--- kibbeh/src/modules/room/useSplitUsersIntoSections.tsx
+++ kibbeh/src/modules/room/useSplitUsersIntoSections.tsx
@@ -65,7 +65,7 @@ export const useSplitUsersIntoSections = ({
         key={u.id}
         src={u.avatarUrl}
         username={u.username}
-        isBot={u.botOwnerId ? true : false}
+        isBot={!!u.botOwnerId}
         activeSpeaker={
           canSpeak && !isMuted && !isDeafened && u.id in activeSpeakerMap
         }
