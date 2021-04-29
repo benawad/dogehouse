85af23a522f13d4347fd5fc3e2aab41ecae7a9d1
amitojsingh366 <amitojsingh366@gmail.com>
2021-04-30 01:42:49 +0300

    fix(kibbeh): @notification/mod only sends if mode state changes in same room

--- kibbeh/src/shared-hooks/useCurrentRoomInfo.ts
+++ kibbeh/src/shared-hooks/useCurrentRoomInfo.ts
@@ -1,11 +1,12 @@
 import isElectron from "is-electron";
 import { useContext } from "react";
+import { string } from "superstruct";
 import { useCurrentRoomIdStore } from "../global-stores/useCurrentRoomIdStore";
 import { isServer } from "../lib/isServer";
 import { WebSocketContext } from "../modules/ws/WebSocketProvider";
 import { useTypeSafeQuery } from "./useTypeSafeQuery";
 
-let iWasMod = false;
+let roomModData: { [id: string]: boolean; };
 let ipcRenderer: any = undefined;
 
 export const useCurrentRoomInfo = () => {
@@ -48,8 +49,14 @@ export const useCurrentRoomInfo = () => {
 
   const isCreator = me.id === data.room.creatorId;
   if (isElectron()) {
-    if (iWasMod !== isMod) {
-      iWasMod = isMod;
+    if (!roomModData) {
+      roomModData = { [currentRoomId]: false };
+    }
+    if (!roomModData[currentRoomId]) {
+      roomModData[currentRoomId] = false;
+    }
+    if (roomModData[currentRoomId] !== isMod) {
+      roomModData[currentRoomId] = isMod;
       ipcRenderer = window.require("electron").ipcRenderer;
       ipcRenderer.send("@notification/mod", isMod);
     }
