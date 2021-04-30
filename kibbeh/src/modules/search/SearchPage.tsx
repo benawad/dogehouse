6ff7e41de984b8aae79c8e0dac73555c4eb95475
GoldyyDev <goldyydev@pm.me>
2021-04-30 20:24:24 +0300

    refactor(kibbeh): made search much better
    The search fields now match the figma design

--- kibbeh/src/modules/search/SearchPage.tsx
+++ kibbeh/src/modules/search/SearchPage.tsx
@@ -1,8 +1,14 @@
+import { Room, User } from "@dogehouse/kebab";
 import router from "next/router";
-import React from "react";
+import React, { useState } from "react";
+import { useWrappedConn } from "../../shared-hooks/useConn";
 import { useScreenType } from "../../shared-hooks/useScreenType";
 import { PageComponent } from "../../types/PageComponent";
-import { PageHeader } from "../../ui/mobile/MobileHeader";
+import { PageHeader, SearchHeader } from "../../ui/mobile/MobileHeader";
+import {
+  RoomSearchResult,
+  UserSearchResult,
+} from "../../ui/Search/SearchResult";
 import { WaitForWsAndAuth } from "../auth/WaitForWsAndAuth";
 import { HeaderController } from "../display/HeaderController";
 import { DefaultDesktopLayout } from "../layouts/DefaultDesktopLayout";
@@ -12,17 +18,56 @@ interface LoungePageProps {}
 
 export const SearchPage: PageComponent<LoungePageProps> = ({}) => {
   const screenType = useScreenType();
-  if(screenType !== "fullscreen") router.push('/dash');
+  if (screenType !== "fullscreen") router.push("/dash");
+
+  const [results, setResults] = useState({ items: [] } as {
+    items: (User | Room)[];
+  });
+  const conn = useWrappedConn();
+
   return (
     <WaitForWsAndAuth>
       <HeaderController embed={{}} title="Search" />
       <DefaultDesktopLayout
         mobileHeader={
-          <PageHeader title={"Search"} onBackClick={() => router.back()} />
+          <SearchHeader
+            onSearchChange={(e) => {
+              console.log(e.target.value);
+              conn.query.search(e.target.value).then((r) => setResults(r));
+            }}
+            searchPlaceholder="Search"
+            onBackClick={() => router.back()}
+          />
         }
       >
-      <div className="mt-5 w-full">
-          <SearchBarController />
+        <div className="h-full w-full">
+          {results &&
+            results.items.map((el, i) => {
+              if ("username" in el) {
+                return (
+                  <UserSearchResult
+                    key={i}
+                    user={{
+                      username: el.username,
+                      displayName: el.displayName,
+                      isOnline: el.online,
+                      avatar: el.avatarUrl,
+                    }}
+                  />
+                );
+              } else {
+                return (
+                  <RoomSearchResult
+                    key={i}
+                    room={{
+                      displayName: el.name,
+                      userCount: el.numPeopleInside,
+                      hosts: el.peoplePreviewList,
+                    }}
+                  />
+                );
+              }
+            })}
         </div>
       </DefaultDesktopLayout>
     </WaitForWsAndAuth>
