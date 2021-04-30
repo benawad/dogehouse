9740e1d724370384546bef302ed793bf2d16d9c5
jasonIV <thuhtetaung.d2@gmail.com>
2021-04-30 19:38:40 +0630

    fix(kibbeh): add formatted title in profile pages
    fix #2442

--- kibbeh/src/modules/user/UserPage.tsx
+++ kibbeh/src/modules/user/UserPage.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React from "react";
 import { PageComponent } from "../../types/PageComponent";
+import { HeaderController } from "../display/HeaderController";
 import { DefaultDesktopLayout } from "../layouts/DefaultDesktopLayout";
 import { MiddlePanel } from "../layouts/GridPanels";
 import { UserProfileController } from "./UserProfileController";
@@ -11,6 +12,11 @@ export const UserPage: PageComponent<UserPageProps> = ({}) => {
   const { query } = useRouter();
   return (
     <DefaultDesktopLayout>
+      <HeaderController
+        title={
+          query.username && !Array.isArray(query.username) ? query.username : ""
+        }
+      />
       <MiddlePanel>
         <UserProfileController
           key={typeof query.username === "string" ? query.username : ""}
