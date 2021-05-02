ecd28bf9f91d92f78df56e9cc1e476b0b5272b15
amitojsingh366 <amitojsingh366@gmail.com>
2021-05-02 19:44:52 +0300

    fix(kousa): remove random trim

--- kousa/lib/kousa/search.ex
+++ kousa/lib/kousa/search.ex
@@ -3,8 +3,6 @@ defmodule Kousa.Search do
   alias Beef.Rooms
 
   def search(query) do
-    case String.trim(query) do
-      Enum.concat(Rooms.search_name(query), Users.search_username(query))
-    end
+    Enum.concat(Rooms.search_name(query), Users.search_username(query))
   end
 end
