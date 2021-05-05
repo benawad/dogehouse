ec1f805970f20bc3027f75f7a139d52fad3f0724
amitojsingh366 <amitojsingh366@gmail.com>
2021-05-05 11:27:13 +0300

    fix(kousa): merge conflicts

--- kousa/lib/broth/message/room/update.ex
+++ kousa/lib/broth/message/room/update.ex
@@ -1,26 +1,14 @@
 defmodule Broth.Message.Room.Update do
   alias Beef.Schemas.Room
 
-<<<<<<< HEAD
-  @derive {Jason.Encoder, only: [:name, :description, :isPrivate, :autoSpeaker, :chatCooldown]}
-
-  @primary_key {:id, :binary_id, []}
-  schema "rooms" do
-    field(:name, :string)
-    field(:description, :string, default: "")
-    field(:isPrivate, :boolean)
-    field(:autoSpeaker, :boolean, virtual: true)
-    field(:chatCooldown, :integer, default: 1, min: 1, virtual: true)
-  end
-=======
   use Broth.Message.Call,
     schema: Room,
     reply: Room
 
   @derive {Jason.Encoder,
-           only: ~w(id name description numPeopleInside isPrivate chatMode autoSpeaker
+           only:
+             ~w(id name description numPeopleInside isPrivate chatMode autoSpeaker chatCooldown
     creatorId voiceServerId inserted_at)a}
->>>>>>> 3e59d04fefe852dfa57f5d30ee0a74171d866b9a
 
   @impl true
   def initialize(state) do
