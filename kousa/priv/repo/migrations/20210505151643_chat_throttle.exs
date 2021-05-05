7c319ce382dc3703ff2ad5836cd257456e65cee9
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-05-05 23:33:32 +0500

    fix(kousa): delete dupe chatThrottle migration

--- kousa/priv/repo/migrations/20210505151643_chat_throttle.exs
+++
@@ -1,9 +0,0 @@
-defmodule Beef.Repo.Migrations.ChatThrottle do
-  use Ecto.Migration
-
-  def change do
-    alter table(:rooms) do
-      add :chatThrottle, :integer, default: 1000, min: 500
-    end
-  end
-end
