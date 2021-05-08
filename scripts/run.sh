0d53a903fea5dd1070e4e7101346013f2d49fe36
Ben Awad <benawad97@gmail.com>
2021-05-08 08:13:43 -0500

    quick setup

---
+++ scripts/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker-compose -f docker-compose.dbs.yml up -d
+yarn install
+cd shawarma
+yarn install
+yarn build
+yarn start &
+cd ../kebab
+yarn build
+cd ../kibbeh
+yarn dev &
+cd ../kousa
+source .envrc
+iex -S mix
