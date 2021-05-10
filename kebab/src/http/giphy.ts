37c8dff260ea82acc307b98cefa976aadf6811c0
Nick Webber <nicholas.e.webber@hotmail.com>
2021-05-10 22:05:49 +0100

    finish gifpicker. I've left an alert to ask other devs how to handle a gif message

--- kebab/src/http/giphy.ts
+++ kebab/src/http/giphy.ts
@@ -4,16 +4,18 @@ import { create } from "../http/raw";
 
 
 export const giphy = () => {
-    const conn = create({
-        baseUrl: 'https://api.giphy.com/v1/gifs/'
-    });
+
     return {
         trendingGifs: () =>
-            conn.request('GET', 'trending?api_key=4bEdDr78WtvmaSX1Ej58K5gRCeKUHq92&limit=25&rating=g') as Promise<{
+            create({
+                baseUrl: 'https://api.giphy.com/v1/gifs/'
+            }).request('GET', 'trending?api_key=4bEdDr78WtvmaSX1Ej58K5gRCeKUHq92&limit=25&rating=g') as Promise<{
                 data: GifResponse[]
             }>,
         queryGifs: (query: string) =>
-            conn.request('GET', `search?api_key=4bEdDr78WtvmaSX1Ej58K5gRCeKUHq92&q=${query}&limit=25&rating=g`) as Promise<{
+            create({
+                baseUrl: 'https://api.giphy.com/v1/gifs/'
+            }).request('GET', `search?api_key=4bEdDr78WtvmaSX1Ej58K5gRCeKUHq92&q=${query}&limit=25&rating=g`) as Promise<{
                 data: GifResponse[]
             }>
     };
