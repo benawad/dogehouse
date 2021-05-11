17cb923bb1fa47406c8240f2367b06850626f4ca
NickersWeb <35151658+NickersWeb@users.noreply.github.com>
2021-05-11 19:36:55 +0100

    Update kebab/src/http/giphy.ts
    Co-authored-by: Ilya Maximov <overlisted@protonmail.ch>

--- kebab/src/http/giphy.ts
+++ kebab/src/http/giphy.ts
@@ -2,16 +2,16 @@ import { GifRating, GifResponse } from "../entities";
 import { create } from "../http/raw";
 
 export const giphy = (apiKey: string) => ({
-        trendingGifs: (limit: number, rating: GifRating) =>
-            create({
-                baseUrl: 'https://api.giphy.com/v1/gifs/'
-            }).request('GET', `trending?api_key=${apiKey}&limit=${limit}&rating=${rating}`) as Promise<{
-                data: GifResponse[]
-            }>,
-        queryGifs: (query: string, limit: number, rating: GifRating) =>
-            create({
-                baseUrl: 'https://api.giphy.com/v1/gifs/'
-            }).request('GET', `search?api_key=${apiKey}&q=${query}&limit=${limit}&rating=${rating}`) as Promise<{
-                data: GifResponse[]
-            }>
+  trendingGifs: (limit: number, rating: GifRating) =>
+    create({
+      baseUrl: 'https://api.giphy.com/v1/gifs/'
+    }).request('GET', `trending?api_key=${apiKey}&limit=${limit}&rating=${rating}`) as Promise<{
+      data: GifResponse[]
+    }>,
+  queryGifs: (query: string, limit: number, rating: GifRating) =>
+    create({
+      baseUrl: 'https://api.giphy.com/v1/gifs/'
+    }).request('GET', `search?api_key=${apiKey}&q=${query}&limit=${limit}&rating=${rating}`) as Promise<{
+      data: GifResponse[]
+    }>
 });
