31453026b57a7a5d5866d82457a7b2011ecfee37
MeztliRA <MeztliRA@gmail.com>
2021-05-11 14:56:22 +0700

    refactor(dolma): use switch instead of if in filterUnitoken.ts

--- dolma/src/lib/filterUnitoken.ts
+++ dolma/src/lib/filterUnitoken.ts
@@ -17,11 +17,18 @@ export function filterUnitoken(token: any): MessageToken | null {
 	const link = token['link'] ?? "https://invalid.link";
 	const text = token['text'] ?? "invalidText";
 
-	if (key == 'mention') return newToken('mention', mention);
-	if (key == 'emote') return newToken("emote", emote);
-	if (key == 'block') return newToken("block", block);
-	if (key == 'link') return newToken("link", link);
-	if (key == 'text') return newToken("text", text);
+  switch (key) {
+    case 'mention':
+      return newToken('mention', mention);
+    case 'emote':
+      return newToken("emote", emote);
+    case 'block':
+      return newToken("block", block);
+    case 'link':
+      return newToken("link", link);
+    case 'text':
+      return newToken("text", text);
+  }
 
 	return null;
 }
