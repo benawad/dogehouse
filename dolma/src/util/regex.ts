91b321f64386d3b7801b9f38a1d847362f8d8a72
GoldyyDev <goldyydev@pm.me>
2021-05-12 20:19:30 +0300

    feat(dolma): dolma improvements
    BREAKING CHANGE: -

--- dolma/src/util/regex.ts
+++ dolma/src/util/regex.ts
@@ -4,7 +4,7 @@ export const validationRegex = {
 	emote: /\:([a-z0-9]+)\:/gi,
 	block: /\`(.*?)\`/gi,
 
-	global: /(\`.*?\`)|(\@[a-zA-Z0-9_]{4,})|(\:[a-z0-9]+\:)|(https?\:\/\/[^ ]+)/gi,
+	global: /(?=(\`.*?\`)|(\@[a-zA-Z0-9_]{4,})|(\:[a-z0-9]+\:)|(https?\:\/\/[^ ]+))/gi,
 
 	unicodeEmoji:  /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])/gm
 }
