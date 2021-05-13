d215964cfcbabfc05c8107a7858433bb2c08c9d6
firecraftgaming <eliyah@sundstroem.com>
2021-05-13 20:31:35 +0000

    feat(kibbeh): added language search

--- kibbeh/src/ui/LanguageSelector.tsx
+++ kibbeh/src/ui/LanguageSelector.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { ParseTextToTwemoji } from "./Twemoji";
 import backIcon from "../icons/SolidCaretRight";
 import { SettingsIcon } from "./SettingsIcon";
+import { LanguageSearch } from "./LanguageSearch";
 import { useRouter } from "next/router";
 
 interface LanguageSelectorProps {
@@ -96,27 +97,35 @@ export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
 
   const { i18n } = useTranslation();
   const { back } = useRouter();
-  const parsedOptions = options.map((e, i) => (
-    <SettingsIcon
-      key={e.value + i}
-      classes={`text-primary-100 focus:outline-no-chrome whitespace-nowrap overflow-ellipsis${
-        i18n.language === e.value ||
-        (e.value === "en" && i18n.language === "en-US")
-          ? " bg-primary-700"
-          : ""
-      }`}
-      onClick={() => {
-        i18n.changeLanguage(e.value);
-        if (mobile) {
-          back();
+  let [parsedOptions, setParsedOptions] = React.useState(getOptions(''));
+
+  function getOptions(search: string) {
+    return options.filter(v => v.label.startsWith(search)).map((e, i) => (
+      <SettingsIcon
+        key={e.value + i}
+        classes={`text-primary-100 focus:outline-no-chrome whitespace-nowrap overflow-ellipsis${
+          i18n.language === e.value ||
+          (e.value === "en" && i18n.language === "en-US")
+            ? " bg-primary-700"
+            : ""
+        }`}
+        onClick={() => {
+          i18n.changeLanguage(e.value);
+          if (mobile) {
+            back();
+          }
+        }}
+        last={i === options.length - 1}
+        icon={
+          <ParseTextToTwemoji text={e.label} style={{ marginRight: "1ch" }} />
         }
-      }}
-      last={i === options.length - 1}
-      icon={
-        <ParseTextToTwemoji text={e.label} style={{ marginRight: "1ch" }} />
-      }
-    ></SettingsIcon>
-  ));
+      ></SettingsIcon>
+    ));
+  }
+
+  function parseOptions(search: string) {
+    setParsedOptions(getOptions(search));
+  }
 
   return (
     <div
@@ -144,11 +153,15 @@ export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
             </div>
           </div>
         )}
+        <LanguageSearch onChange={v => parseOptions(v)}/>
         <div
           className="block h-full overflow-y-auto scrollbar-thin scrollbar-thumb-primary-700 overflow-x-hidden mb-9 md:pb-0"
           style={{ height: mobile ? "auto" : "calc(100% - 40px)" }}
         >
-          <div className="block">{parsedOptions}</div>
+          <div className="block">
+            
+            {parsedOptions}
+          </div>
         </div>
       </div>
     </div>
