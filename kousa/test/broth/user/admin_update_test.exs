512c077a934144df0618528c0241e679e2e5ab5f
Ben Awad <benawad97@gmail.com>
2021-05-12 22:46:36 -0500

    test(kousa): fix

--- kousa/test/broth/user/admin_update_test.exs
+++ kousa/test/broth/user/admin_update_test.exs
@@ -56,10 +56,14 @@ defmodule BrothTest.User.AdminUpdateTest do
       assert %{staff: true, contributions: 100} = Users.get_by_id(t.staff_user.id)
     end
 
-    test "works for staff", t do
+    test "staff can update staff", t do
+      staff = Factory.create(User, staff: true)
+      staff_ws = WsClientFactory.create_client_for(staff)
+      should_become_staff = Factory.create(User)
+
       ref =
-        WsClient.send_call(t.client_ws, "user:admin_update", %{
-          "username" => t.staff_user.username,
+        WsClient.send_call(staff_ws, "user:admin_update", %{
+          "username" => should_become_staff.username,
           "user" => %{
             "staff" => true,
             "contributions" => 100
@@ -72,26 +76,7 @@ defmodule BrothTest.User.AdminUpdateTest do
       })
 
       # check that the user has been updated.
-      assert %{staff: true, contributions: 100} = Users.get_by_id(t.staff_user.id)
-
-      staffed = Factory.create(User)
-
-      ref =
-        WsClient.send_call(t.staff_client_ws, "user:admin_update", %{
-          "username" => staffed.username,
-          "user" => %{
-            "staff" => true,
-            "contributions" => 100
-          }
-        })
-
-      WsClient.assert_reply("user:admin_update:reply", ref, %{
-        "staff" => true,
-        "contributions" => 100
-      })
-
-      # check that the user has been updated.
-      assert %{staff: true, contributions: 100} = Users.get_by_id(staffed.id)
+      assert %{staff: true, contributions: 100} = Users.get_by_id(should_become_staff.id)
     end
 
     test "can update single field", t do
