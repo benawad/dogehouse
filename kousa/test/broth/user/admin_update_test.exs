8c4479915212d5b5f4cb24a521d297fc25e8175a
Amitoj Singh <amitojsingh366@gmail.com>
2021-05-13 03:38:57 +0300

    chore(kousa): update tests

--- kousa/test/broth/user/admin_update_test.exs
+++ kousa/test/broth/user/admin_update_test.exs
@@ -14,9 +14,10 @@ defmodule BrothTest.User.AdminUpdateTest do
 
   setup do
     user = Factory.create(User, githubId: @ben_github_id)
+    staff_user = Factory.create(User)
 
     client_ws = WsClientFactory.create_client_for(user)
-    staff_client_ws = WsClientFactory.create_client_for(user)
+    staff_client_ws = WsClientFactory.create_client_for(staff_user)
 
     {:ok,
      user: user, staff_user: staff_user, staff_client_ws: staff_client_ws, client_ws: client_ws}
