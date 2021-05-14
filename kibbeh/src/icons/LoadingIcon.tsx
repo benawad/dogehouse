4b008db06103add08f2b2a9c93429da1b75ef02b
akashrajum7 <akashrajum7@gmail.com>
2021-05-14 22:23:06 +0530

    fix(kibbeh): fix linting error

--- kibbeh/src/icons/LoadingIcon.tsx
+++ kibbeh/src/icons/LoadingIcon.tsx
@@ -14,7 +14,7 @@ function LoadingIcon(props: React.SVGProps<SVGSVGElement>) {
         cy="12"
         r="10"
         stroke="currentColor"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
