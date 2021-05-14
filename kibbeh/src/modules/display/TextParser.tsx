da73742ba1fbe4d84298622ae86fcaaaa00e8931
beamop <beamop@pm.me>
2021-05-14 19:26:21 +0200

    fix(kibbeh): updated text parser regex
    Safari doesn't support lookbehind
    
    Fix #2725

--- kibbeh/src/modules/display/TextParser.tsx
+++ kibbeh/src/modules/display/TextParser.tsx
@@ -9,9 +9,9 @@ interface TextParserProps {
 
 export const TextParser: React.FC<TextParserProps> = ({ children }) => {
   return (<>
-    {children.split(/(?=[ ,\n])|(?<=[ ,\n])/g).map((text, i) => {
+    {children.split(/(?=[ ,\n])/g).map((text, i) => {
       if (new RegExp(linkRegex).test(text)) return <a key={i} className={"text-accent text-center hover:underline inline font-bold"} href={text}>{text}</a>;
-      if (emojiRegex().test(text)) return <ParseTextToTwemoji key={i} text={text}/>;
+      if (emojiRegex().test(text)) return <ParseTextToTwemoji key={i} text={text} />;
       return text;
     })}
   </>
