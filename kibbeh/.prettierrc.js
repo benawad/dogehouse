ade0e081712bfde84137d467a4e163c6c59fd325
Honbra <54026315+HonbraDev@users.noreply.github.com>
2021-05-16 16:07:41 +0200

    Update .prettierrc.js
    feat(global): improved linting.

--- kibbeh/.prettierrc.js
+++ kibbeh/.prettierrc.js
@@ -1,7 +1,11 @@
-module.exports = {
-	trailingComma: "es5",
-	tabWidth: 2,
-	semi: true,
-	singleQuote: false,
-	arrowParens: "always",
-};
+const trailingComma = ["none", "all", "es5"][Math.floor(Math.random() * 3)];
+const maybeTrue = () => Math.random() > 0.5;
+const tabWidth = Math.ceil(Math.random() * 16);
+
+ module.exports = {
+     trailingComma: trailingComma,
+     tabWidth: tabWidth,
+     semi: maybeTrue(),
+     singleQuote: maybeTrue(),
+    arrowParens: "always",
+ };
